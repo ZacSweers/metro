0ec3d077f10ba72e0f22064bd3a455f42b279f30
Zac Sweers <pandanomic@gmail.com>
2024-11-05 17:45:08 -0500

    Split out test utils

--- compiler/src/test/kotlin/dev/zacsweers/lattice/compiler/transformers/InjectConstructorTransformerTest.kt
+++ compiler/src/test/kotlin/dev/zacsweers/lattice/compiler/transformers/InjectConstructorTransformerTest.kt
@@ -15,12 +15,9 @@
  */
 package dev.zacsweers.lattice.compiler.transformers
 
-import com.google.common.truth.Truth.assertThat
-import com.tschuchort.compiletesting.JvmCompilationResult
 import com.tschuchort.compiletesting.SourceFile.Companion.kotlin
 import dev.zacsweers.lattice.compiler.LatticeCompilerTest
-import dev.zacsweers.lattice.internal.Factory
-import java.util.concurrent.Callable
+import dev.zacsweers.lattice.compiler.assertCallableFactory
 import org.junit.Ignore
 import org.junit.Test
 
@@ -74,33 +71,4 @@ class InjectConstructorTransformerTest : LatticeCompilerTest() {
       )
     result.assertCallableFactory("Hello, world!")
   }
-}
-
-fun JvmCompilationResult.assertCallableFactory(value: String) {
-  val factory = ExampleClass.generatedFactoryClass()
-  val callable = factory.invokeNewInstanceAs<Callable<String>>(value)
-  assertThat(callable.call()).isEqualTo(value)
-}
-
-val JvmCompilationResult.ExampleClass: Class<*>
-  get() {
-    return classLoader.loadClass("test.ExampleClass")
-  }
-
-fun Class<*>.generatedFactoryClass(): Class<Factory<*>> {
-  @Suppress("UNCHECKED_CAST")
-  return classLoader.loadClass(name + "_Factory") as Class<Factory<*>>
-}
-
-fun Class<Factory<*>>.invokeNewInstance(vararg args: Any): Any {
-  return declaredMethods.single { it.name == "newInstance" }.invoke(null, *args)
-}
-
-fun <T> Class<Factory<*>>.invokeNewInstanceAs(vararg args: Any): T {
-  @Suppress("UNCHECKED_CAST")
-  return invokeNewInstance(*args) as T
-}
-
-fun Class<Factory<*>>.invokeCreate(vararg args: Any): Factory<*> {
-  return declaredMethods.single { it.name == "create" }.invoke(null, *args) as Factory<*>
 }
