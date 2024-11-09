beb8c01aad4c1a29629d44aa80fbf3f879bb20ba
Zac Sweers <pandanomic@gmail.com>
2024-11-09 03:46:35 -0500

    Improved type keys

--- compiler/src/main/kotlin/dev/zacsweers/lattice/transformers/TypeKey.kt
+++ compiler/src/main/kotlin/dev/zacsweers/lattice/transformers/TypeKey.kt
@@ -1,22 +1,36 @@
 package dev.zacsweers.lattice.transformers
 
 import dev.zacsweers.lattice.ir.IrAnnotation
+import dev.zacsweers.lattice.unsafeLazy
 import org.jetbrains.kotlin.ir.declarations.IrFunction
 import org.jetbrains.kotlin.ir.declarations.IrValueParameter
 import org.jetbrains.kotlin.ir.types.IrType
+import org.jetbrains.kotlin.ir.util.render
 
 internal data class TypeKey(val type: IrType, val qualifier: IrAnnotation? = null) {
-  companion object {
-    fun from(context: LatticeTransformerContext, parameter: IrFunction): TypeKey =
-      TypeKey(
-        parameter.returnType,
-        with(context) { parameter.qualifierAnnotation() },
-      )
+  private val cachedToString by unsafeLazy {
+    buildString {
+      qualifier?.let {
+        append("@" + it.ir.render())
+        append(" ")
+      }
+      append(type.render())
+    }
+  }
 
-    fun from(context: LatticeTransformerContext, parameter: IrValueParameter): TypeKey =
-      TypeKey(
-        parameter.type,
-        with(context) { parameter.qualifierAnnotation() },
-      )
+  override fun toString(): String = cachedToString
+
+  companion object {
+    fun from(
+      context: LatticeTransformerContext,
+      function: IrFunction,
+      type: IrType = function.returnType,
+    ): TypeKey = TypeKey(type, with(context) { function.qualifierAnnotation() })
+
+    fun from(
+      context: LatticeTransformerContext,
+      parameter: IrValueParameter,
+      type: IrType = parameter.type,
+    ): TypeKey = TypeKey(type, with(context) { parameter.qualifierAnnotation() })
   }
 }
