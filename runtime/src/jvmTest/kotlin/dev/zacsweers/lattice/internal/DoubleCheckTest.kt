38b318e24c03711f0650d8aa716825f5743982d7
Zac Sweers <pandanomic@gmail.com>
2024-11-30 01:51:01 -0500

    Replace atomics

--- runtime/src/jvmTest/kotlin/dev/zacsweers/lattice/internal/DoubleCheckTest.kt
+++ runtime/src/jvmTest/kotlin/dev/zacsweers/lattice/internal/DoubleCheckTest.kt
@@ -22,13 +22,14 @@ import dev.zacsweers.lattice.Provider
 import java.util.concurrent.Callable
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.Executors
-import java.util.concurrent.atomic.AtomicInteger
-import java.util.concurrent.atomic.AtomicReference
 import kotlin.test.Test
 import kotlin.test.assertFailsWith
+import kotlinx.atomicfu.atomic
 
 // TODO convert to coroutines test instead?
 class DoubleCheckTest {
+  val doubleCheckReference = atomic<Provider<Any>?>(null)
+  val invocationCount = atomic(0)
 
   @Test
   fun `double wrapping provider`() {
@@ -61,7 +62,7 @@ class DoubleCheckTest {
 
     val futures = executor.invokeAll(tasks)
 
-    assertThat(provider.provisions.get()).isEqualTo(1)
+    assertThat(provider.provisions.value).isEqualTo(1)
     val results: MutableSet<Any> = Sets.newIdentityHashSet()
     for (future in futures) {
       results.add(future.get())
@@ -70,7 +71,7 @@ class DoubleCheckTest {
   }
 
   private class LatchedProvider(val latch: CountDownLatch?) : Provider<Any> {
-    val provisions = AtomicInteger()
+    val provisions = atomic(0)
 
     override fun invoke(): Any {
       if (latch != null) {
@@ -83,50 +84,45 @@ class DoubleCheckTest {
 
   @Test
   fun `reentrance without condition throws stack overflow`() {
-    val doubleCheckReference: AtomicReference<Provider<Any>> = AtomicReference()
-    val doubleCheck: Provider<Any> = DoubleCheck.provider(Provider { doubleCheckReference.get()() })
-    doubleCheckReference.set(doubleCheck)
+    val doubleCheck = DoubleCheck.provider(Provider { doubleCheckReference.value!!.invoke() })
+    doubleCheckReference.value = doubleCheck
     assertFailsWith<StackOverflowError> { doubleCheck() }
   }
 
   @Test
   fun `reentrance returning same instance`() {
-    val doubleCheckReference: AtomicReference<Provider<Any>> = AtomicReference()
-    val invocationCount = AtomicInteger()
     val obj = Any()
-    val doubleCheck: Provider<Any> =
+    val doubleCheck =
       DoubleCheck.provider(
         Provider {
           if (invocationCount.incrementAndGet() == 1) {
-            doubleCheckReference.get()()
+            doubleCheckReference.value!!.invoke()
           }
           obj
         }
       )
-    doubleCheckReference.set(doubleCheck)
+    doubleCheckReference.value = doubleCheck
     assertThat(doubleCheck()).isSameInstanceAs(obj)
   }
 
   @Test
   fun `reentrance returning different instances throws IllegalStateException`() {
-    val doubleCheckReference = AtomicReference<Provider<Any>>()
-    val invocationCount = AtomicInteger()
     val doubleCheck =
       DoubleCheck.provider(
         Provider {
           if (invocationCount.incrementAndGet() == 1) {
-            doubleCheckReference.get()()
+            doubleCheckReference.value!!.invoke()
           }
           Any()
         }
       )
-    doubleCheckReference.set(doubleCheck)
+    doubleCheckReference.value = doubleCheck
     assertFailsWith<IllegalStateException> { doubleCheck() }
   }
 
   @Test
   fun `instance factory as lazy does not wrap`() {
-    val factory: Factory<Any> = InstanceFactory.create(Any())
+    val factory = InstanceFactory.create(Any())
     assertThat(DoubleCheck.lazy(factory)).isSameInstanceAs(factory)
   }
 
