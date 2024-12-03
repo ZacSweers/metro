93adcbbb1fa56f35730232d309443717c8a37e46
Zac Sweers <pandanomic@gmail.com>
2024-12-03 12:49:18 -0500

    Fix rendering

--- compiler/src/main/kotlin/dev/zacsweers/lattice/fir/LatticeFirAnnotation.kt
+++ compiler/src/main/kotlin/dev/zacsweers/lattice/fir/LatticeFirAnnotation.kt
@@ -1,15 +1,29 @@
+/*
+ * Copyright (C) 2024 Zac Sweers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package dev.zacsweers.lattice.fir
 
 import dev.zacsweers.lattice.appendIterableWith
 import dev.zacsweers.lattice.unsafeLazy
 import org.jetbrains.kotlin.fir.expressions.FirAnnotationCall
 import org.jetbrains.kotlin.fir.expressions.FirExpression
+import org.jetbrains.kotlin.fir.expressions.FirLiteralExpression
 import org.jetbrains.kotlin.fir.expressions.arguments
 import org.jetbrains.kotlin.fir.types.classId
 import org.jetbrains.kotlin.fir.types.resolvedType
-import org.jetbrains.kotlin.ir.expressions.IrConst
-import org.jetbrains.kotlin.ir.expressions.IrConstKind
-import org.jetbrains.kotlin.ir.symbols.UnsafeDuringIrConstructionAPI
+import org.jetbrains.kotlin.types.ConstantValueKind
 
 internal class LatticeFirAnnotation(val fir: FirAnnotationCall) {
   private val cachedHashKey by unsafeLazy { fir.computeAnnotationHash() }
@@ -40,7 +54,6 @@ internal class LatticeFirAnnotation(val fir: FirAnnotationCall) {
   override fun toString() = cachedToString
 }
 
-@OptIn(UnsafeDuringIrConstructionAPI::class)
 private fun StringBuilder.renderAsAnnotation(firAnnotation: FirAnnotationCall) {
   val annotationClassName = firAnnotation.resolvedType.classId?.asString() ?: "<unbound>"
   append(annotationClassName)
@@ -60,27 +73,28 @@ private fun StringBuilder.renderAsAnnotation(firAnnotation: FirAnnotationCall) {
 }
 
 private fun StringBuilder.renderAsAnnotationArgument(argument: FirExpression) {
-  println(argument)
-  //  when (argument) {
-  //    null -> append("<null>")
-  //    is IrConstructorCall -> renderAsAnnotation(irElement)
-  //    is IrConst -> {
-  //      renderIrConstAsAnnotationArgument(irElement)
-  //    }
-  //    is IrVararg -> {
-  //      appendIterableWith(irElement.elements, prefix = "[", postfix = "]", separator = ", ") {
-  //        renderAsAnnotationArgument(it)
-  //      }
-  //    }
-  //    else -> append("...")
-  //  }
+  when (argument) {
+    // TODO
+    //      is IrConstructorCall -> renderAsAnnotation(irElement)
+    is FirLiteralExpression -> {
+      renderFirLiteralAsAnnotationArgument(argument)
+    }
+    // TODO
+    //      is IrVararg -> {
+    //        appendIterableWith(irElement.elements, prefix = "[", postfix = "]", separator = ", ")
+    // {
+    //          renderAsAnnotationArgument(it)
+    //        }
+    //      }
+    else -> append("...")
+  }
 }
 
-private fun StringBuilder.renderIrConstAsAnnotationArgument(const: IrConst) {
+private fun StringBuilder.renderFirLiteralAsAnnotationArgument(const: FirLiteralExpression) {
   val quotes =
     when (const.kind) {
-      IrConstKind.String -> "\""
-      IrConstKind.Char -> "'"
+      ConstantValueKind.Char -> "'"
+      ConstantValueKind.String -> "\""
       else -> ""
     }
   append(quotes)
