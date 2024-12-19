7a8dce2a822f1a56a07d76408ca0a2b83bfc999c
Zac Sweers <pandanomic@gmail.com>
2024-12-19 18:07:33 -0500

    Extract LatticeFirCheckers.kt

--- compiler/src/main/kotlin/dev/zacsweers/lattice/fir/LatticeFirExtensionRegistrar.kt
+++ compiler/src/main/kotlin/dev/zacsweers/lattice/fir/LatticeFirExtensionRegistrar.kt
@@ -16,15 +16,6 @@
 package dev.zacsweers.lattice.fir
 
 import dev.zacsweers.lattice.LatticeClassIds
-import dev.zacsweers.lattice.fir.checkers.AssistedInjectChecker
-import dev.zacsweers.lattice.fir.checkers.ComponentCreatorChecker
-import dev.zacsweers.lattice.fir.checkers.InjectConstructorChecker
-import dev.zacsweers.lattice.fir.checkers.ProvidesChecker
-import org.jetbrains.kotlin.fir.FirSession
-import org.jetbrains.kotlin.fir.analysis.checkers.declaration.DeclarationCheckers
-import org.jetbrains.kotlin.fir.analysis.checkers.declaration.FirCallableDeclarationChecker
-import org.jetbrains.kotlin.fir.analysis.checkers.declaration.FirClassChecker
-import org.jetbrains.kotlin.fir.analysis.extensions.FirAdditionalCheckersExtension
 import org.jetbrains.kotlin.fir.extensions.FirExtensionRegistrar
 
 internal class LatticeFirExtensionRegistrar(private val latticeClassIds: LatticeClassIds) :
@@ -32,29 +23,4 @@ internal class LatticeFirExtensionRegistrar(private val latticeClassIds: Lattice
   override fun ExtensionRegistrarContext.configurePlugin() {
     +LatticeFirCheckers.getFactory(latticeClassIds)
   }
-}
-
-internal class LatticeFirCheckers(
-  session: FirSession,
-  private val latticeClassIds: LatticeClassIds,
-) : FirAdditionalCheckersExtension(session) {
-  companion object {
-    fun getFactory(latticeClassIds: LatticeClassIds) = Factory { session ->
-      LatticeFirCheckers(session, latticeClassIds)
-    }
-  }
-
-  override val declarationCheckers: DeclarationCheckers =
-    object : DeclarationCheckers() {
-      override val classCheckers: Set<FirClassChecker>
-        get() =
-          setOf(
-            InjectConstructorChecker(session, latticeClassIds),
-            AssistedInjectChecker(session, latticeClassIds),
-            ComponentCreatorChecker(session, latticeClassIds),
-          )
-
-      override val callableDeclarationCheckers: Set<FirCallableDeclarationChecker>
-        get() = setOf(ProvidesChecker(session, latticeClassIds))
-    }
 }
