1f3eecde18b2ca6cbcf48c6dd30fd289844b13d4
Zac Sweers <pandanomic@gmail.com>
2025-01-27 00:20:00 -0500

    basic sample

--- samples/basic/src/commonMain/kotlin/dev/zacsweers/metro/sample/ExampleGraph.kt
+++ samples/basic/src/commonMain/kotlin/dev/zacsweers/metro/sample/ExampleGraph.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package dev.zacsweers.lattice.sample
+package dev.zacsweers.metro.sample
 
 import dev.zacsweers.lattice.BindsInstance
 import dev.zacsweers.lattice.DependencyGraph
