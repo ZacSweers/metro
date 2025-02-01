3e553f9e1adfc631d104129e16aa80b083230c5f
Zac Sweers <pandanomic@gmail.com>
2025-01-31 20:37:02 -0500

    Formatting + updated license headers

--- samples/weather-app/src/commonMain/kotlin/dev/zacsweers/metro/sample/weather/AppGraph.kt
+++ samples/weather-app/src/commonMain/kotlin/dev/zacsweers/metro/sample/weather/AppGraph.kt
@@ -1,22 +1,8 @@
-/*
- * Copyright (C) 2025 Zac Sweers
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright (C) 2025 Zac Sweers
+// SPDX-License-Identifier: Apache-2.0
 package dev.zacsweers.metro.sample.weather
 
 import dev.zacsweers.metro.AppScope
-import dev.zacsweers.metro.DependencyGraph
 import dev.zacsweers.metro.Provides
 import dev.zacsweers.metro.SingleIn
 import io.ktor.client.HttpClient
@@ -34,10 +20,7 @@ interface AppGraph {
 
   @SingleIn(AppScope::class)
   @Provides
-  fun httpClient(
-    httpClientEngineFactory: HttpClientEngineFactory<*>,
-    json: Json,
-  ): HttpClient =
+  fun httpClient(httpClientEngineFactory: HttpClientEngineFactory<*>, json: Json): HttpClient =
     HttpClient(httpClientEngineFactory) {
       expectSuccess = true
       install(HttpRequestRetry) {
