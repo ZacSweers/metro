c921e066809bd6fab24c708e89ec6c5f774a0071
Zac Sweers <pandanomic@gmail.com>
2025-02-04 02:53:53 -0500

    Updated docs

--- samples/weather-app/src/jvmMain/kotlin/dev/zacsweers/metro/sample/weather/JvmAppGraph.kt
+++ samples/weather-app/src/jvmMain/kotlin/dev/zacsweers/metro/sample/weather/JvmAppGraph.kt
@@ -6,4 +6,4 @@ import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.DependencyGraph
 import dev.zacsweers.metro.SingleIn
 
-@SingleIn(AppScope::class) @DependencyGraph(AppScope::class) interface JvmAppGraph : AppGraph
+@DependencyGraph(AppScope::class) interface JvmAppGraph : AppGraph
