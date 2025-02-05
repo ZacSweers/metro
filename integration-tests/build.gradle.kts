3a32473b68ccff4295cfead5600faa39fc0743fb
Zac Sweers <pandanomic@gmail.com>
2025-02-05 11:40:24 -0500

    Work around noise

--- integration-tests/build.gradle.kts
+++ integration-tests/build.gradle.kts
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
 
 plugins {
   alias(libs.plugins.kotlin.multiplatform)
@@ -21,11 +22,26 @@ kotlin {
     commonTest {
       dependencies {
         implementation(libs.kotlin.test)
-        // For Platform use
+        // For PlatformUtils use
         implementation(libs.ktor.client)
       }
     }
   }
+
+  targets
+    .matching {
+      it.platformType == KotlinPlatformType.js || it.platformType == KotlinPlatformType.wasm
+    }
+    .configureEach {
+      compilations.configureEach {
+        compileTaskProvider.configure {
+          // These are all read at compile-time
+          compilerOptions.freeCompilerArgs.add(
+            "-Xsuppress-warning=RUNTIME_ANNOTATION_NOT_SUPPORTED"
+          )
+        }
+      }
+    }
 }
 
 metro { debug.set(false) }
