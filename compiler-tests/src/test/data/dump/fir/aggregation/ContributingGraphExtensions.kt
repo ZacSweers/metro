27f9407dc1a4cfb3eeaae86139b425a9d49469ca
Zac Sweers <pandanomic@gmail.com>
2025-04-21 14:46:50 -0400

    Start a test

---
+++ compiler-tests/src/test/data/dump/fir/aggregation/ContributingGraphExtensions.kt
@@ -0,0 +1,12 @@
+abstract class LoggedInScope
+
+@ContributesGraphExtension(LoggedInScope::class)
+interface LoggedInGraph {
+  @ContributesGraphExtension.Factory(AppScope::class)
+  interface Factory {
+    fun createLoggedInGraph()
+  }
+}
+
+@DependencyGraph(scope = AppScope::class)
+interface ExampleGraph
