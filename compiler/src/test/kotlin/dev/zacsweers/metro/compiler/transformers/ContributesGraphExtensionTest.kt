dd6b1ac707b28b52b2b59d018837f3c9a95616d8
Zac Sweers <pandanomic@gmail.com>
2025-04-21 22:51:30 -0400

    More tests

--- compiler/src/test/kotlin/dev/zacsweers/metro/compiler/transformers/ContributesGraphExtensionTest.kt
+++ compiler/src/test/kotlin/dev/zacsweers/metro/compiler/transformers/ContributesGraphExtensionTest.kt
@@ -1,6 +1,8 @@
 package dev.zacsweers.metro.compiler.transformers
 
 import com.google.common.truth.Truth.assertThat
+import com.tschuchort.compiletesting.KotlinCompilation
+import com.tschuchort.compiletesting.addPreviousResultToClasspath
 import dev.zacsweers.metro.compiler.ExampleGraph
 import dev.zacsweers.metro.compiler.MetroCompilerTest
 import dev.zacsweers.metro.compiler.callFunction
@@ -38,6 +40,119 @@ class ContributesGraphExtensionTest : MetroCompilerTest() {
       val exampleGraph = ExampleGraph.generatedMetroGraphClass().createGraphWithNoArgs()
       val loggedInGraph = exampleGraph.callFunction<Any>("createLoggedInGraph")
       assertThat(loggedInGraph.callProperty<Int>("int")).isEqualTo(0)
+    }
+  }
+
+  @Test
+  fun `multiple modules`() {
+    val firstCompilation =
+      compile(
+        source(
+          """
+          abstract class LoggedInScope
+          
+          @ContributesGraphExtension(LoggedInScope::class)
+          interface LoggedInGraph {
+            val int: Int
+          
+            @ContributesGraphExtension.Factory(AppScope::class)
+            interface Factory {
+              fun createLoggedInGraph(): LoggedInGraph
+            }
+          }
+        """
+            .trimIndent()
+        )
+      )
+
+    compile(
+      source(
+        """
+          @DependencyGraph(scope = AppScope::class, isExtendable = true)
+          interface ExampleGraph {
+            @Provides fun provideInt(): Int = 0
+          }
+        """
+          .trimIndent()
+      ),
+      previousCompilationResult = firstCompilation,
+    ) {
+      val exampleGraph = ExampleGraph.generatedMetroGraphClass().createGraphWithNoArgs()
+      val loggedInGraph = exampleGraph.callFunction<Any>("createLoggedInGraph")
+      assertThat(loggedInGraph.callProperty<Int>("int")).isEqualTo(0)
+    }
+  }
+
+  @Test
+  fun `multiple modules including contributed`() {
+    val loggedInScope =
+      compile(
+        source(
+          """
+          abstract class LoggedInScope
+        """
+            .trimIndent()
+        )
+      )
+
+    val loggedInGraph =
+      compile(
+        source(
+          """
+          @ContributesGraphExtension(LoggedInScope::class)
+          interface LoggedInGraph {
+            val string: String
+          
+            @ContributesGraphExtension.Factory(AppScope::class)
+            interface Factory {
+              fun createLoggedInGraph(): LoggedInGraph
+            }
+          }
+        """
+            .trimIndent()
+        ),
+        previousCompilationResult = loggedInScope,
+      )
+
+    val contributor =
+      compile(
+        source(
+          """
+          @ContributesTo(LoggedInScope::class)
+          interface LoggedInStringProvider {
+            @Provides fun provideString(int: Int): String = int.toString()
+          }
+        """
+            .trimIndent()
+        ),
+        previousCompilationResult = loggedInScope,
+      )
+
+    val compilation =
+      prepareCompilation(
+          source(
+            """
+          @DependencyGraph(scope = AppScope::class, isExtendable = true)
+          interface ExampleGraph {
+            @Provides fun provideInt(): Int = 0
+          }
+        """
+              .trimIndent()
+          ),
+          debug = true,
+        )
+        .apply {
+          addPreviousResultToClasspath(loggedInScope)
+          addPreviousResultToClasspath(loggedInGraph)
+          addPreviousResultToClasspath(contributor)
+        }
+
+    val result = compilation.compile()
+    with(result) {
+      assertThat(exitCode).isEqualTo(KotlinCompilation.ExitCode.OK)
+      val exampleGraph = ExampleGraph.generatedMetroGraphClass().createGraphWithNoArgs()
+      val loggedInGraph = exampleGraph.callFunction<Any>("createLoggedInGraph")
+      assertThat(loggedInGraph.callProperty<String>("string")).isEqualTo("0")
     }
   }
 }
