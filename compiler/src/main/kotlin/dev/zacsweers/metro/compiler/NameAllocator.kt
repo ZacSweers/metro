3baf92922c45ad2c239ee2d949346c90f6493dfa
Zac Sweers <pandanomic@gmail.com>
2025-04-24 00:48:52 -0400

    Cleanup

--- compiler/src/main/kotlin/dev/zacsweers/metro/compiler/NameAllocator.kt
+++ compiler/src/main/kotlin/dev/zacsweers/metro/compiler/NameAllocator.kt
@@ -125,7 +125,7 @@ private constructor(
    * [NameAllocator.get].
    */
   fun newName(suggestion: String, tag: Any = Uuid.random().toString()): String {
-    var result = buildString { append(dev.zacsweers.metro.compiler.toJavaIdentifier(suggestion)) }
+    var result = buildString { append(toJavaIdentifier(suggestion)) }
     var count = 1
     while (!allocatedNames.add(result)) {
       count++
@@ -157,8 +157,8 @@ private constructor(
    *
    * @return A deep copy of this NameAllocator.
    */
-  fun copy(): dev.zacsweers.metro.compiler.NameAllocator {
-    return dev.zacsweers.metro.compiler.NameAllocator(
+  fun copy(): NameAllocator {
+    return NameAllocator(
       allocatedNames.toMutableSet(),
       tagToName.toMutableMap(),
       mode = mode,
@@ -194,7 +194,7 @@ private fun toJavaIdentifier(suggestion: String) = buildString {
   }
 }
 
-internal fun dev.zacsweers.metro.compiler.NameAllocator.newName(
+internal fun NameAllocator.newName(
   suggestion: Name,
   tag: Any = Uuid.random().toString(),
 ): Name {
