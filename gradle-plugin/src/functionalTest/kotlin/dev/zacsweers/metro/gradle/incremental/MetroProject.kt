b518ee3536d570f60b49330a0ba66767b5884e82
Zac Sweers <pandanomic@gmail.com>
2025-04-25 00:03:23 -0400

    Add debug option

--- gradle-plugin/src/functionalTest/kotlin/dev/zacsweers/metro/gradle/incremental/MetroProject.kt
+++ gradle-plugin/src/functionalTest/kotlin/dev/zacsweers/metro/gradle/incremental/MetroProject.kt
@@ -5,7 +5,7 @@ import com.autonomousapps.kit.GradleProject
 import com.autonomousapps.kit.GradleProject.DslKind
 import com.autonomousapps.kit.Source
 
-abstract class MetroProject : AbstractGradleProject() {
+abstract class MetroProject(private val debug: Boolean = false) : AbstractGradleProject() {
   protected abstract fun sources(): List<Source>
 
   val gradleProject: GradleProject
@@ -13,7 +13,17 @@ abstract class MetroProject : AbstractGradleProject() {
       newGradleProjectBuilder(DslKind.KOTLIN)
         .withRootProject {
           sources = this@MetroProject.sources()
-          withBuildScript { plugins(GradlePlugins.Kotlin.jvm, GradlePlugins.metro) }
+          withBuildScript {
+            plugins(GradlePlugins.Kotlin.jvm, GradlePlugins.metro)
+            if (debug) {
+              withKotlin(
+                """
+                metro { debug.set(true) }
+                """
+                  .trimIndent()
+              )
+            }
+          }
         }
         .write()
 }
