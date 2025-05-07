a35fb7b9522036dda494faa39d59d24139e64e77
Zac Sweers <pandanomic@gmail.com>
2025-05-06 21:40:47 -0400

    Add more

--- compiler/src/test/kotlin/dev/zacsweers/metro/compiler/ir/TracingTest.kt
+++ compiler/src/test/kotlin/dev/zacsweers/metro/compiler/ir/TracingTest.kt
@@ -35,13 +35,6 @@ class TracingTest : MetroCompilerTest() {
       options = metroOptions.copy(reportsDestination = reportsDir, debug = true),
     ) {
       val timings = reportsDir.resolve("timings.csv").readText()
-      // tag,description,durationMs
-      // test.ExampleGraph,Build DependencyGraphNode,15
-      // test.ExampleGraph,Implement creator functions,0
-      // test.ExampleGraph,Build binding graph,1
-      // test.ExampleGraph,-- Validate binding graph,4
-      // test.ExampleGraph,Transform metro graph,10
-      // test.ExampleGraph,Transform dependency graph,46
       val withoutTime = timings.lines().drop(1).joinToString("\n") { it.substringBeforeLast(",") }
       assertThat(withoutTime)
         .isEqualTo(
@@ -82,6 +75,231 @@ class TracingTest : MetroCompilerTest() {
                 ◀ Implement overrides (xx ms)
               ◀ Transform metro graph (xx ms)
             [ExampleGraph] ◀ Transform dependency graph (xx ms)
+          """
+            .trimIndent()
+        )
+    }
+  }
+
+  @Test
+  fun `graph extensions nest`() {
+    val reportsDir = temporaryFolder.newFolder("reports").toPath()
+    compile(
+      source(
+        """
+            @DependencyGraph(AppScope::class, isExtendable = true)
+            interface ExampleGraph {
+              @DependencyGraph.Factory
+              fun interface Factory {
+                fun create(@Provides text: String): ExampleGraph
+              }
+            }
+
+            @DependencyGraph(Unit::class)
+            interface ChildGraph {
+
+              fun exampleClass(): ExampleClass
+              
+              @DependencyGraph.Factory
+              fun interface Factory {
+                fun create(@Extends parent: ExampleGraph): ChildGraph
+              }
+            }
+
+            @Inject
+            class ExampleClass(private val text: String) : Callable<String> {
+              override fun call(): String = text
+            }
+          """
+          .trimIndent()
+      ),
+      options = metroOptions.copy(reportsDestination = reportsDir, debug = true),
+    ) {
+      val timings = reportsDir.resolve("timings.csv").readText()
+      val withoutTime = timings.lines().drop(1).joinToString("\n") { it.substringBeforeLast(",") }
+      assertThat(withoutTime)
+        .isEqualTo(
+          """
+          ExampleGraph,Build DependencyGraphNode
+          ExampleGraph,Implement creator functions
+          ExampleGraph,Build binding graph
+          ExampleGraph,Validate binding graph
+          ExampleGraph,Collect bindings
+          ExampleGraph,Compute safe init order
+          ExampleGraph,Implement overrides
+          ExampleGraph,Generate Metro metadata
+          ExampleGraph,Transform metro graph
+          ExampleGraph,Transform dependency graph
+          ChildGraph,Build DependencyGraphNode
+          ChildGraph,Implement creator functions
+          ChildGraph,Build binding graph
+          ChildGraph,Validate binding graph
+          ChildGraph,Collect bindings
+          ChildGraph,Compute safe init order
+          ChildGraph,Implement overrides
+          ChildGraph,Transform metro graph
+          ChildGraph,Transform dependency graph
+        """
+            .trimIndent()
+        )
+
+      val traceLog = reportsDir.resolve("traceLog.txt").readText()
+      val cleanedLog = traceLog.replace("\\((\\d+) ms\\)".toRegex(), "(xx ms)")
+      assertThat(cleanedLog.trim())
+        .isEqualTo(
+          """
+            [ExampleGraph] ▶ Transform dependency graph
+              ▶ Build DependencyGraphNode
+              ◀ Build DependencyGraphNode (xx ms)
+              ▶ Implement creator functions
+              ◀ Implement creator functions (xx ms)
+              ▶ Build binding graph
+              ◀ Build binding graph (xx ms)
+              ▶ Validate binding graph
+              ◀ Validate binding graph (xx ms)
+              ▶ Transform metro graph
+                ▶ Collect bindings
+                ◀ Collect bindings (xx ms)
+                ▶ Compute safe init order
+                ◀ Compute safe init order (xx ms)
+                ▶ Implement overrides
+                ◀ Implement overrides (xx ms)
+                ▶ Generate Metro metadata
+                ◀ Generate Metro metadata (xx ms)
+              ◀ Transform metro graph (xx ms)
+            [ExampleGraph] ◀ Transform dependency graph (xx ms)
+            [ChildGraph] ▶ Transform dependency graph
+              ▶ Build DependencyGraphNode
+              ◀ Build DependencyGraphNode (xx ms)
+              ▶ Implement creator functions
+              ◀ Implement creator functions (xx ms)
+              ▶ Build binding graph
+              ◀ Build binding graph (xx ms)
+              ▶ Validate binding graph
+              ◀ Validate binding graph (xx ms)
+              ▶ Transform metro graph
+                ▶ Collect bindings
+                ◀ Collect bindings (xx ms)
+                ▶ Compute safe init order
+                ◀ Compute safe init order (xx ms)
+                ▶ Implement overrides
+                ◀ Implement overrides (xx ms)
+              ◀ Transform metro graph (xx ms)
+            [ChildGraph] ◀ Transform dependency graph (xx ms)
+          """
+            .trimIndent()
+        )
+    }
+  }
+
+  @Test
+  fun `contributed extension`() {
+    val reportsDir = temporaryFolder.newFolder("reports").toPath()
+    compile(
+      source(
+        """
+            @DependencyGraph(AppScope::class, isExtendable = true)
+            interface ExampleGraph {
+              @DependencyGraph.Factory
+              fun interface Factory {
+                fun create(@Provides text: String): ExampleGraph
+              }
+            }
+
+            @ContributesGraphExtension(Unit::class)
+            interface ChildGraph {
+
+              fun exampleClass(): ExampleClass
+              
+              @ContributesGraphExtension.Factory(AppScope::class)
+              fun interface Factory {
+                fun createChildGraph(): ChildGraph
+              }
+            }
+
+            @Inject
+            class ExampleClass(private val text: String) : Callable<String> {
+              override fun call(): String = text
+            }
+          """
+          .trimIndent()
+      ),
+      options = metroOptions.copy(reportsDestination = reportsDir, debug = true),
+    ) {
+      val timings = reportsDir.resolve("timings.csv").readText()
+      val withoutTime = timings.lines().drop(1).joinToString("\n") { it.substringBeforeLast(",") }
+      assertThat(withoutTime)
+        .isEqualTo(
+          """
+          ExampleGraph,Build DependencyGraphNode
+          ExampleGraph,Implement creator functions
+          ExampleGraph,Build binding graph
+          ExampleGraph,Validate binding graph
+          ExampleGraph,Collect bindings
+          ExampleGraph,Compute safe init order
+          ExampleGraph,Generate contributed graph ChildGraph
+          ExampleGraph,Implement overrides
+          ExampleGraph,Generate Metro metadata
+          ExampleGraph,Transform metro graph
+          ExampleGraph,Transform dependency graph
+          $${'$'}ContributedChildGraph,Build DependencyGraphNode
+          $${'$'}ContributedChildGraph,Implement creator functions
+          $${'$'}ContributedChildGraph,Build binding graph
+          $${'$'}ContributedChildGraph,Validate binding graph
+          $${'$'}ContributedChildGraph,Collect bindings
+          $${'$'}ContributedChildGraph,Compute safe init order
+          $${'$'}ContributedChildGraph,Implement overrides
+          $${'$'}ContributedChildGraph,Transform metro graph
+          $${'$'}ContributedChildGraph,Transform dependency graph
+        """
+            .trimIndent()
+        )
+
+      val traceLog = reportsDir.resolve("traceLog.txt").readText()
+      val cleanedLog = traceLog.replace("\\((\\d+) ms\\)".toRegex(), "(xx ms)")
+      assertThat(cleanedLog.trim())
+        .isEqualTo(
+          """
+            [ExampleGraph] ▶ Transform dependency graph
+              ▶ Build DependencyGraphNode
+              ◀ Build DependencyGraphNode (xx ms)
+              ▶ Implement creator functions
+              ◀ Implement creator functions (xx ms)
+              ▶ Build binding graph
+              ◀ Build binding graph (xx ms)
+              ▶ Validate binding graph
+              ◀ Validate binding graph (xx ms)
+              ▶ Transform metro graph
+                ▶ Collect bindings
+                ◀ Collect bindings (xx ms)
+                ▶ Compute safe init order
+                ◀ Compute safe init order (xx ms)
+                ▶ Implement overrides
+                  ▶ Generate contributed graph ChildGraph
+                  ◀ Generate contributed graph ChildGraph (xx ms)
+                ◀ Implement overrides (xx ms)
+                ▶ Generate Metro metadata
+                ◀ Generate Metro metadata (xx ms)
+              ◀ Transform metro graph (xx ms)
+            [ExampleGraph] ◀ Transform dependency graph (xx ms)
+            [$${'$'}ContributedChildGraph] ▶ Transform dependency graph
+              ▶ Build DependencyGraphNode
+              ◀ Build DependencyGraphNode (xx ms)
+              ▶ Implement creator functions
+              ◀ Implement creator functions (xx ms)
+              ▶ Build binding graph
+              ◀ Build binding graph (xx ms)
+              ▶ Validate binding graph
+              ◀ Validate binding graph (xx ms)
+              ▶ Transform metro graph
+                ▶ Collect bindings
+                ◀ Collect bindings (xx ms)
+                ▶ Compute safe init order
+                ◀ Compute safe init order (xx ms)
+                ▶ Implement overrides
+                ◀ Implement overrides (xx ms)
+              ◀ Transform metro graph (xx ms)
+            [$${'$'}ContributedChildGraph] ◀ Transform dependency graph (xx ms)
           """
             .trimIndent()
         )
