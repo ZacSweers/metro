82e12cfc6c8f62eca998de35637ae4134a716251
Zac Sweers <pandanomic@gmail.com>
2025-05-13 18:36:35 -0400

    Harden sorting + update some cycle tests

--- compiler/src/main/kotlin/dev/zacsweers/metro/compiler/graph/BindingGraph.kt
+++ compiler/src/main/kotlin/dev/zacsweers/metro/compiler/graph/BindingGraph.kt
@@ -180,8 +180,8 @@ internal open class MutableBindingGraph<
           onCycle = { cycle ->
             val fullCycle =
               buildList {
-                  add(cycle.last())
                   addAll(cycle)
+                  add(cycle.first())
                 }
                 // Reverse upfront so we can backward look at dependency requests
                 .reversed()
@@ -191,14 +191,13 @@ internal open class MutableBindingGraph<
                 .mapIndexed { i, key ->
                   val callingBinding =
                     if (i == 0) {
-                      // This is the first index, must be an entry-point instead (i.e. "requested
-                      // by")
-                      null
+                      // This is the first index, back around to the back
+                      bindings.getValue(fullCycle[fullCycle.lastIndex - 1])
                     } else {
                       bindings.getValue(fullCycle[i - 1])
                     }
                   stack.newBindingStackEntry(
-                    callingBinding?.dependencies?.firstOrNull { it.typeKey == key }
+                    callingBinding.dependencies.firstOrNull { it.typeKey == key }
                       ?: bindings.getValue(key).contextualTypeKey,
                     callingBinding,
                     roots,
@@ -234,15 +233,21 @@ internal open class MutableBindingGraph<
         }
       }
 
+      val entriesToReport = if (fullCycle.size == 2) {
+        fullCycle.take(1)
+      } else {
+        fullCycle
+      }
+
       appendLine()
       appendLine()
       // Print the full stack
       appendLine("Trace:")
       appendBindingStackEntries(
         stack.graphFqName,
-        fullCycle,
+        entriesToReport,
         indent = indent,
-        ellipse = fullCycle.size > 1,
+        ellipse = entriesToReport.size > 1,
         short = false,
       )
     }
