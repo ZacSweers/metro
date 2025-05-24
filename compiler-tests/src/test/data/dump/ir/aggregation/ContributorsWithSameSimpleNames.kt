ddd8e5e25862942ee8e4253514f1e376c6bf2c53
Zac Sweers <pandanomic@gmail.com>
2025-05-25 00:08:39 +0200

    Comment

--- compiler-tests/src/test/data/dump/ir/aggregation/ContributorsWithSameSimpleNames.kt
+++ compiler-tests/src/test/data/dump/ir/aggregation/ContributorsWithSameSimpleNames.kt
@@ -14,5 +14,7 @@ interface ContributedInterface
 
 // MODULE: main(lib)
 
+// Note the compiler test framework is ok with generating duplicate IR files, so we do this
+// IR dump test to golden image it to watch for regressions
 @DependencyGraph(scope = AppScope::class)
 interface ExampleGraph
