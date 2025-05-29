e8cbeb066d8b0ec639c06904033e35a26988e2a6
Zac Sweers <pandanomic@gmail.com>
2025-05-29 03:02:38 +0100

    Finish tests

--- compiler-tests/src/test/data/dump/ir/injectconstructor/ConstructorFunctionSignatures.kt
+++ compiler-tests/src/test/data/dump/ir/injectconstructor/ConstructorFunctionSignatures.kt
@@ -3,8 +3,8 @@
 
 @Inject class SimpleClass<T>
 @Inject class ClassWithBounds<T : Any>
-@Inject class ClassWithNullable<T?>
+@Inject class ClassWithNullable<T, E : T?>
 @Inject class ClassWithMultiple<T, V, E>
 @Inject class ClassWithBackRefs<T, V, E : V>
 @Inject class ClassWithWheres<T, V, E> where E : V
-@Inject class ComplexMonster<T, V, E, LongName?> where E : V, V : T, T : Any
+@Inject class ComplexMonster<T, V, E, LongName, NullableLongName : LongName?> where E : V, V : T, T : Any
