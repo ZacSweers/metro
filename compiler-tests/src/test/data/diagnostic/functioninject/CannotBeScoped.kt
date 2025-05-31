95631981f8f166cb31d227a4ef257f18162424d9
Zac Sweers <pandanomic@gmail.com>
2025-05-31 13:35:22 +0100

    Implement top-level function injection checkers

---
+++ compiler-tests/src/test/data/diagnostic/functioninject/CannotBeScoped.kt
@@ -0,0 +1,6 @@
+// RENDER_DIAGNOSTICS_FULL_TEXT
+<!FUNCTION_INJECT_ERROR!>@SingleIn(AppScope::class)<!>
+@Inject
+fun Example() {
+
+}
---
+++ compiler-tests/src/test/data/diagnostic/functioninject/CannotHaveExtensionReceivers.kt
@@ -0,0 +1,5 @@
+// RENDER_DIAGNOSTICS_FULL_TEXT
+@Inject
+fun <!FUNCTION_INJECT_ERROR!>String<!>.Example() {
+
+}
