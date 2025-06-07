6ed550cadd1db8e6efee717ba37f4cdfca47a7d3
Zac Sweers <pandanomic@gmail.com>
2025-06-07 22:18:03 +0100

    Add a terrifying test

--- compiler-tests/src/test/data/box/inject/member/GenericMemberInjectionWithDeepAncesors.kt
+++ compiler-tests/src/test/data/box/inject/member/GenericMemberInjectionWithDeepAncesors.kt
@@ -1,5 +1,15 @@
+abstract class GrandParent<T : Any, R : Any> {
+  @Inject lateinit var grandParentT: T
+  @Inject lateinit var grandParentR: R
+}
+
+abstract class Parent<T : Any, R : Any> : GrandParent<R, T>() {
+  @Inject lateinit var parentT: T
+  @Inject lateinit var parentR: R
+}
+
 @Inject
-class ExampleClass<T : Any> {
+class ExampleClass<T : Any> : Parent<T, String>() {
   @Inject lateinit var value: T
   @Inject lateinit var values: List<T>
   @Inject lateinit var mapValues: Map<T, List<T>>
@@ -20,6 +30,8 @@ class ExampleClass<T : Any> {
 interface AppGraph {
   val exampleClass: ExampleClass<Int>
 
+  @Provides fun provideString(): String = "Hello, world!"
+
   @Provides fun provideInt(): Int = 3
 
   @Provides fun provideInts(): List<Int> = listOf(3)
@@ -35,5 +47,9 @@ fun box(): String {
   assertEquals(exampleClass.values, listOf(3))
   assertEquals(exampleClass.mapValues, mapOf(3 to listOf(3)))
   assertEquals(exampleClass.functionSet, 3)
+  assertEquals(exampleClass.parentT, 3)
+  assertEquals(exampleClass.parentR, "Hello, world!")
+  assertEquals(exampleClass.grandParentT, "Hello, world!")
+  assertEquals(exampleClass.grandParentR, 3)
   return "OK"
 }
