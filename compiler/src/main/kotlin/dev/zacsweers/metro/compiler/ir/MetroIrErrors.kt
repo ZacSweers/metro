33a72a9abcc6a2fbbd29459155b22c26044a2693
Zac Sweers <pandanomic@gmail.com>
2025-06-11 15:50:31 +0100

    Extract diagnostics

--- compiler/src/main/kotlin/dev/zacsweers/metro/compiler/ir/MetroIrErrors.kt
+++ compiler/src/main/kotlin/dev/zacsweers/metro/compiler/ir/MetroIrErrors.kt
@@ -1,5 +1,18 @@
 package dev.zacsweers.metro.compiler.ir
 
-//internal object MetroIrErrors : KtDiagnosticsContainer() {
-//
-//}
+import org.jetbrains.kotlin.diagnostics.BackendErrors
+import org.jetbrains.kotlin.diagnostics.KtDiagnosticFactoryToRendererMap
+import org.jetbrains.kotlin.diagnostics.SourceElementPositioningStrategies
+import org.jetbrains.kotlin.diagnostics.error0
+import org.jetbrains.kotlin.diagnostics.rendering.BaseDiagnosticRendererFactory
+
+internal object MetroIrErrors {
+  val NON_LOCAL_RETURN_IN_DISABLED_INLINE by error0<PsiElement>(SourceElementPositioningStrategies.DEFAULT) // need to reference SourceElementPositioningStrategies at least once to initialize properly
+
+}
+
+internal object MetroIrErrorMessages : BaseDiagnosticRendererFactory() {
+  override val MAP by KtDiagnosticFactoryToRendererMap("BackendErrors") { map ->
+    map.put(BackendErrors.NON_LOCAL_RETURN_IN_DISABLED_INLINE, "Non-local returns are not allowed with inlining disabled")
+  }
+}
