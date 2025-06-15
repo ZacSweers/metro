b7f6728b56e0baeb23ac7c2b828223d6700c6074
Zac Sweers <pandanomic@gmail.com>
2025-06-15 19:46:08 -0400

    Remove lazy

--- compiler-tests/src/test/data/box/cycles/AssistedFactoryCycle.kt
+++ compiler-tests/src/test/data/box/cycles/AssistedFactoryCycle.kt
@@ -2,11 +2,9 @@
 class Foo(
   @Assisted nested: Boolean,
   factoryProvider: Provider<Factory>,
-  factoryLazy: Lazy<Factory>,
   factory: Factory,
 ) {
   val nestedFooViaProvider = if (nested) factoryProvider().create() else null
-  val nestedFooViaLazy = if (nested) factoryLazy.value.create() else null
   val nestedFoo = if (nested) factory.create() else null
 
   @AssistedFactory
@@ -24,10 +22,8 @@ fun box(): String {
   val cycleGraph = createGraph<CycleGraph>()
   val foo = cycleGraph.fooFactory().create(nested = true)
   assertNotNull(foo.nestedFooViaProvider)
-  assertNotNull(foo.nestedFooViaLazy)
   assertNotNull(foo.nestedFoo)
   assertNull(foo.nestedFooViaProvider?.nestedFooViaProvider)
-  assertNull(foo.nestedFooViaLazy?.nestedFooViaLazy)
   assertNull(foo.nestedFoo?.nestedFoo)
   return "OK"
 }
