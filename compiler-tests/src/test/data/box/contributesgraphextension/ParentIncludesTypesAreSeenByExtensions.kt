33fe216b46b4e2ebaea91a25e597da8d8fef4d9c
Kevin Chiu <kevin85619@gmail.com>
2025-06-16 18:14:03 +0800

    Test is failing!

--- compiler-tests/src/test/data/box/contributesgraphextension/ParentIncludesTypesAreSeenByExtensions.kt
+++ compiler-tests/src/test/data/box/contributesgraphextension/ParentIncludesTypesAreSeenByExtensions.kt
@@ -1,46 +1,48 @@
-// MODULE: lib
 interface FeatureScope
 
-@DependencyGraph(FeatureScope::class, isExtendable = true)
+interface IntProvider {
+  val int: Int
+}
+
+@DependencyGraph(scope = FeatureScope::class, isExtendable = true)
 interface FeatureGraph {
 
   @DependencyGraph.Factory
   interface Factory {
     fun create(
-      @Includes serviceProvider: FeatureServiceProvider
+      @Includes serviceProvider: FeatureServiceProvider,
     ): FeatureGraph
+  }
+}
+
+@ContributesGraphExtension(scope = Unit::class)
+interface FeatureSubGraph {
+
+  @ContributesGraphExtension.Factory(scope = FeatureScope::class)
+  interface Factory {
+    fun create(): FeatureSubGraph
   }
 }
 
 @ContributesTo(AppScope::class)
 interface FeatureServiceProvider {
-  val int: Int
+  fun intProvider(): IntProvider
 }
 
-@ContributesGraphExtension(scope = Unit::class)
-interface LoggedInGraph {
-  val int: Int
-
-  @ContributesGraphExtension.Factory(FeatureScope::class)
-  interface Factory {
-    fun createLoggedInGraph(
-      @Provides str: String
-    ): LoggedInGraph
-  }
-}
-
-// MODULE: main(lib)
 @DependencyGraph(AppScope::class)
-interface AppGraph {
-  @Provides
-  fun provideInt(): Int = 3
+interface AppGraph
+
+@Inject
+@ContributesBinding(AppScope::class)
+class IntProviderImpl : IntProvider {
+  override val int: Int get() = 3
 }
 
 fun box(): String {
   val parent = createGraph<AppGraph>()
   val feature = createGraphFactory<FeatureGraph.Factory>()
     .create(parent.asContribution<FeatureServiceProvider>())
-  val child = feature.createLoggedInGraph("str")
-  assertEquals(child.int, 3)
+  val child = feature.create()
+  assertNotNull(child)
   return "OK"
 }
