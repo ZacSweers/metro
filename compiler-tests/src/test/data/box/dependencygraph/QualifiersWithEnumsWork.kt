d51f7861a0f2c7b0eed154751501ad9975f43186
Zac Sweers <pandanomic@gmail.com>
2025-07-10 10:52:32 -0400

    Handle enum values in renderAsAnnotationArgument()

--- compiler-tests/src/test/data/box/dependencygraph/QualifiersWithEnumsWork.kt
+++ compiler-tests/src/test/data/box/dependencygraph/QualifiersWithEnumsWork.kt
@@ -1,5 +1,9 @@
 // https://github.com/ZacSweers/metro/issues/690
-enum class AuthScope { UNAUTHENTICATED, ACCOUNT, PROFILE }
+enum class AuthScope {
+  UNAUTHENTICATED,
+  ACCOUNT,
+  PROFILE,
+}
 
 @Qualifier
 @Retention(AnnotationRetention.RUNTIME)
@@ -7,13 +11,17 @@ annotation class ForAuthScope(val scope: AuthScope)
 
 @DependencyGraph
 interface AppGraph {
-  @ForAuthScope(AuthScope.UNAUTHENTICATED) val id: Int
+  @ForAuthScope(AuthScope.UNAUTHENTICATED) val unauthedId: Int
+  @ForAuthScope(AuthScope.ACCOUNT) val accountId: Int
 
-  @Provides @ForAuthScope(AuthScope.UNAUTHENTICATED) fun provideId(): Int = 3
+  @Provides @ForAuthScope(AuthScope.UNAUTHENTICATED) fun provideUnauthedId(): Int = 3
+
+  @Provides @ForAuthScope(AuthScope.ACCOUNT) fun provideAccountId(): Int = 4
 }
 
 fun box(): String {
   val graph = createGraph<AppGraph>()
-  assertEquals(3, graph.id)
+  assertEquals(3, graph.unauthedId)
+  assertEquals(4, graph.accountId)
   return "OK"
 }
