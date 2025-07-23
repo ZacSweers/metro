899c58ff823ce9b00ac7759e601887456ead7e72
Zac Sweers <pandanomic@gmail.com>
2025-07-22 22:22:13 -0400

    Fix main and update node/yarn versions
    This whole system is weird

--- samples/build.gradle.kts
+++ samples/build.gradle.kts
@@ -4,6 +4,12 @@ import com.diffplug.gradle.spotless.SpotlessExtension
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompilerOptions
 import org.jetbrains.kotlin.gradle.plugin.KotlinBasePlugin
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsEnvSpec
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootEnvSpec
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 
 plugins {
@@ -83,5 +89,20 @@ subprojects {
         }
       }
     }
+  }
+}
+
+plugins.withType<YarnPlugin> {
+  the<YarnRootEnvSpec>().apply {
+    version = "1.22.22"
+    yarnLockAutoReplace = true
+    installationDirectory = projectDir
+    ignoreScripts = false
+  }
+}
+
+plugins.withType<NodeJsRootPlugin> {
+  the<NodeJsEnvSpec>().apply {
+    this.version = "24.4.1"
   }
 }
