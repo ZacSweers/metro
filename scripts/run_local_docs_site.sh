d39c3d6a850f0f962a20a76adc00082be7248639
Hossain Khan <hello@hossain.dev>
2025-09-18 13:39:59 +0000

    Simplify script to always run local docs server

--- scripts/run_local_docs_site.sh
+++ scripts/run_local_docs_site.sh
@@ -4,50 +4,13 @@
 # https://squidfunk.github.io/mkdocs-material/
 # It requires Python to run.
 # Install the packages with the following command:
-# pip install mkdocs mkdocs-material mdx_truly_sane_lists "mkdocs-material[imaging]" mike
+# pip install -r .github/workflows/mkdocs-requirements.txt
 #
 # To run the site locally with hot-reload support, use:
-# ./deploy_website.sh --local
-
-if [[ "$1" = "--local" ]]; then local=true; fi
-
-if ! [[ ${local} ]]; then
-  set -ex
-
-  export GIT_CLONE_PROTECTION_ACTIVE=false
-  REPO="git@github.com:zacsweers/metro.git"
-  DIR=temp-clone
-
-  # Delete any existing temporary website clone
-  rm -rf ${DIR}
-
-  # Clone the current repo into temp folder
-  git clone ${REPO} ${DIR}
-
-  # Move working directory into temp folder
-  cd ${DIR}
-
-  # Generate API docs using shared script
-  ./scripts/generate_docs_dokka.sh
-
-  cd ..
-  rm -rf ${DIR}
-  rm -rf site
-fi
+# ./run_local_docs_site.sh
 
 # Copy documentation files using shared script
 ./scripts/copy_docs_files.sh
 
-# Build the site and push the new files up to GitHub
-if ! [[ ${local} ]]; then
-  mkdocs gh-deploy
-else
-  mkdocs serve
-fi
-
-# Delete our temp folder
-if ! [[ ${local} ]]; then
-  cd ..
-  rm -rf ${DIR}
-  rm -rf site
-fi
+# Serve the site locally with hot-reload
+mkdocs serve
