ab864c3420776bfd6a85ebd0b86abf584c95792e
Zac Sweers <pandanomic@gmail.com>
2025-10-05 12:43:20 -0400

    Notes

--- compiler-compat/k230_dev_7984/src/main/kotlin/dev/zacsweers/metro/compiler/compat/k230_dev_7984/FirCompatContextImpl.kt
+++ compiler-compat/k230_dev_7984/src/main/kotlin/dev/zacsweers/metro/compiler/compat/k230_dev_7984/FirCompatContextImpl.kt
@@ -1,4 +1,4 @@
-package dev.zacsweers.metro.compiler.compat.k230_dev_9673
+package dev.zacsweers.metro.compiler.compat.k230_dev_7984
 
 import dev.zacsweers.metro.compiler.compat.FirCompatContext
 import org.jetbrains.kotlin.fir.FirSession
@@ -8,6 +8,7 @@ import org.jetbrains.kotlin.fir.symbols.FirBasedSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirCallableSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirClassLikeSymbol
 
+// Ships in 2025.3-EAP
 public class FirCompatContextImpl : FirCompatContext {
   override fun FirBasedSymbol<*>.getContainingClassSymbol(): FirClassLikeSymbol<*>? {
     return moduleData.session.firProvider.getContainingClass(this)
@@ -21,7 +22,7 @@ public class FirCompatContextImpl : FirCompatContext {
   override fun FirDeclaration.getContainingClassSymbol(): FirClassLikeSymbol<*>? = symbol.getContainingClassSymbol()
 
   public class Factory : FirCompatContext.Factory {
-    override val minVersion: String = "2.3.0-dev-9673"
+    override val minVersion: String = "2.3.0-dev-7984"
 
     override fun create(): FirCompatContext = FirCompatContextImpl()
   }
