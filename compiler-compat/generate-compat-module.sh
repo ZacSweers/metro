69de8a2340a6eb1dbdf1d62f92a83bbae162523a
Zac Sweers <pandanomic@gmail.com>
2025-10-06 12:22:45 -0400

    Spotless

--- compiler-compat/generate-compat-module.sh
+++ compiler-compat/generate-compat-module.sh
@@ -34,9 +34,7 @@ mkdir -p "$MODULE_DIR/src/main/resources/META-INF/services"
 cat > "$MODULE_DIR/build.gradle.kts" << EOF
 // Copyright (C) 2025 Zac Sweers
 // SPDX-License-Identifier: Apache-2.0
-plugins {
-  alias(libs.plugins.kotlin.jvm)
-}
+plugins { alias(libs.plugins.kotlin.jvm) }
 
 kotlin {
   compilerOptions {
--- compiler-compat/k2220/build.gradle.kts
+++ compiler-compat/k2220/build.gradle.kts
@@ -1,8 +1,6 @@
 // Copyright (C) 2025 Zac Sweers
 // SPDX-License-Identifier: Apache-2.0
-plugins {
-  alias(libs.plugins.kotlin.jvm)
-}
+plugins { alias(libs.plugins.kotlin.jvm) }
 
 kotlin {
   compilerOptions {
