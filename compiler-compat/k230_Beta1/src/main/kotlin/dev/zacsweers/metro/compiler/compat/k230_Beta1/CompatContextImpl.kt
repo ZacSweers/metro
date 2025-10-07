4068374a8901a35734ce6f5e779c1897f2295a6d
Zac Sweers <pandanomic@gmail.com>
2025-10-07 14:29:50 -0400

    Delegate to the other impl

--- compiler-compat/k230_Beta1/src/main/kotlin/dev/zacsweers/metro/compiler/compat/k230_Beta1/CompatContextImpl.kt
+++ compiler-compat/k230_Beta1/src/main/kotlin/dev/zacsweers/metro/compiler/compat/k230_Beta1/CompatContextImpl.kt
@@ -3,12 +3,11 @@
 package dev.zacsweers.metro.compiler.compat.k230_Beta1
 
 import dev.zacsweers.metro.compiler.compat.CompatContext
+import dev.zacsweers.metro.compiler.compat.k230_dev_7984.CompatContextImpl as K230Dev7984Impl
 
-public class CompatContextImpl : CompatContext {
-  // TODO Implement
-
+public class CompatContextImpl : CompatContext by K230Dev7984Impl() {
   public class Factory : CompatContext.Factory {
-    override val kotlinVersion: String = "2.3.0-Beta1"
+    override val minVersion: String = "2.3.0-Beta1"
 
     override fun create(): CompatContext = CompatContextImpl()
   }
