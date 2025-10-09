11f9659779f8a0a3cacc5382f9ccbb9feb1346d3
Zac Sweers <pandanomic@gmail.com>
2025-10-09 15:01:24 -0400

    qualifier tests

--- compiler-tests/src/test/data/box/interop/dagger/BindsOptionalOfInterop.kt
+++ compiler-tests/src/test/data/box/interop/dagger/BindsOptionalOfInterop.kt
@@ -5,21 +5,31 @@ import dagger.Component
 import dagger.Module
 import java.util.Optional
 
-// TODO qualifiers, multi-module
+// TODO multi-module
 
 @Module
 interface Bindings {
   @BindsOptionalOf
   fun optionalString(): String
 
+  @Named("qualified")
+  @BindsOptionalOf
+  fun qualifiedOptionalString(): String
+
   @BindsOptionalOf
   fun optionalInt(): Int
+
+  @Named("qualified")
+  @BindsOptionalOf
+  fun qualifiedOptionalInt(): Int
 }
 
 @Component(modules = [Bindings::class])
 interface ExampleGraph {
   val emptyOptionalAccessor: Optional<String>
+  @Named("qualified") val qualifiedEmptyOptionalAccessor: Optional<String>
   val presentOptionalAccessor: Optional<Int>
+  @Named("qualified") val qualifiedPresentOptionalAccessor: Optional<Int>
   val stringConsumer: StringConsumer
 
   @Provides fun provideInt(): Int = 3
@@ -32,7 +42,9 @@ class StringConsumer @Inject constructor(
 fun box(): String {
   val graph = createGraph<ExampleGraph>()
   assertTrue(graph.emptyOptionalAccessor.isEmpty())
+  assertTrue(graph.qualifiedEmptyOptionalAccessor.isEmpty())
   assertEquals(3, graph.presentOptionalAccessor.get())
+  assertTrue(graph.qualifiedPresentOptionalAccessor.isEmpty())
   assertTrue(graph.stringConsumer.value.isEmpty())
   return "OK"
 }
