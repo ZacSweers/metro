cf8c4c783be460847ac2553c6f9837e89815a75d
Zac Sweers <pandanomic@gmail.com>
2025-10-09 15:04:49 -0400

    Multi-module

--- compiler-tests/src/test/data/box/interop/dagger/BindsOptionalOfInterop.kt
+++ compiler-tests/src/test/data/box/interop/dagger/BindsOptionalOfInterop.kt
@@ -1,11 +1,24 @@
 // ENABLE_DAGGER_INTEROP
+// MODULE: lib
+import dagger.BindsOptionalOf
+import dagger.Module
+
+@Module
+interface ExternalBindings {
+  @BindsOptionalOf
+  fun optionalLong(): Long
+
+  @Named("qualified")
+  @BindsOptionalOf
+  fun qualifiedOptionalLong(): Long
+}
+
+// MODULE: main(lib)
 import javax.inject.Inject
 import dagger.BindsOptionalOf
 import dagger.Component
 import dagger.Module
 import java.util.Optional
-
-// TODO multi-module
 
 @Module
 interface Bindings {
@@ -24,15 +37,18 @@ interface Bindings {
   fun qualifiedOptionalInt(): Int
 }
 
-@Component(modules = [Bindings::class])
+@Component(modules = [Bindings::class, ExternalBindings::class])
 interface ExampleGraph {
   val emptyOptionalAccessor: Optional<String>
   @Named("qualified") val qualifiedEmptyOptionalAccessor: Optional<String>
   val presentOptionalAccessor: Optional<Int>
-  @Named("qualified") val qualifiedPresentOptionalAccessor: Optional<Int>
+  @Named("qualified") val qualifiedEmptyIntAccessor: Optional<Int>
+  val presentOptionalLongAccessor: Optional<Long>
+  @Named("qualified") val qualifiedEmptyLongAccessor: Optional<Long>
   val stringConsumer: StringConsumer
 
   @Provides fun provideInt(): Int = 3
+  @Provides fun provideLong(): Long = 3L
 }
 
 class StringConsumer @Inject constructor(
@@ -44,7 +60,9 @@ fun box(): String {
   assertTrue(graph.emptyOptionalAccessor.isEmpty())
   assertTrue(graph.qualifiedEmptyOptionalAccessor.isEmpty())
   assertEquals(3, graph.presentOptionalAccessor.get())
-  assertTrue(graph.qualifiedPresentOptionalAccessor.isEmpty())
+  assertTrue(graph.qualifiedEmptyIntAccessor.isEmpty())
+  assertEquals(3L, graph.presentOptionalLongAccessor.get())
+  assertTrue(graph.qualifiedEmptyLongAccessor.isEmpty())
   assertTrue(graph.stringConsumer.value.isEmpty())
   return "OK"
 }
