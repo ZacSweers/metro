ace1b865aa468bec076a8f80899d604680d632b0
Zac Sweers <pandanomic@gmail.com>
2025-10-09 13:05:16 -0400

    WIP test

---
+++ compiler-tests/src/test/data/box/interop/dagger/BindsOptionalOfInterop.kt
@@ -0,0 +1,38 @@
+// ENABLE_DAGGER_INTEROP
+import javax.inject.Inject
+import dagger.BindsOptionalOf
+import dagger.Component
+import dagger.Module
+import java.util.Optional
+
+// TODO qualifiers, multi-module
+
+@Module
+interface Bindings {
+  @BindsOptionalOf
+  fun optionalString(): String
+
+  @BindsOptionalOf
+  fun optionalInt(): Int
+}
+
+@Component(modules = [Bindings::class])
+interface ExampleGraph {
+  val emptyOptionalAccessor: Optional<String>
+  val presentOptionalAccessor: Optional<Int>
+//  val stringConsumer: StringConsumer
+
+  @Provides fun provideInt(): Int = 3
+}
+
+//class StringConsumer @Inject constructor(
+//  val value: Optional<String>
+//)
+
+fun box(): String {
+  val graph = createGraph<ExampleGraph>()
+  assertTrue(graph.emptyOptionalAccessor.isEmpty())
+  assertEquals(3, graph.presentOptionalAccessor.get())
+//  assertTrue(graph.stringConsumer.value.isEmpty())
+  return "OK"
+}
