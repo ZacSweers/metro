0050237fab5c729557515fb752051277fbebd1d2
Zac Sweers <pandanomic@gmail.com>
2025-10-09 13:04:21 -0400

    Optional utils

--- compiler/src/main/kotlin/dev/zacsweers/metro/compiler/ir/IrOptionalExpressionGenerator.kt
+++ compiler/src/main/kotlin/dev/zacsweers/metro/compiler/ir/IrOptionalExpressionGenerator.kt
@@ -1,10 +1,17 @@
 package dev.zacsweers.metro.compiler.ir
 
+import dev.zacsweers.metro.compiler.Symbols
+import dev.zacsweers.metro.compiler.fir.MetroDiagnostics
 import org.jetbrains.kotlin.ir.builders.IrBuilderWithScope
+import org.jetbrains.kotlin.ir.declarations.IrDeclaration
 import org.jetbrains.kotlin.ir.expressions.IrExpression
 import org.jetbrains.kotlin.ir.symbols.IrFunctionSymbol
+import org.jetbrains.kotlin.ir.types.IrStarProjection
 import org.jetbrains.kotlin.ir.types.IrType
+import org.jetbrains.kotlin.ir.types.IrTypeProjection
 import org.jetbrains.kotlin.ir.types.typeWith
+import org.jetbrains.kotlin.ir.util.classId
+import org.jetbrains.kotlin.name.ClassId
 
 internal class IrOptionalExpressionGenerator(context: IrMetroContext) : IrMetroContext by context {
   /** Generates an `Optional.empty()` call. */
@@ -36,4 +43,31 @@ internal class IrOptionalExpressionGenerator(context: IrMetroContext) : IrMetroC
       }
       irInvoke(callee = callee, args = listOf(instanceExpression), typeHint = typeHint)
     }
+}
+
+context(context: IrMetroContext)
+internal fun IrType.optionalKind(declaration: IrDeclaration?): Pair<OptionalKind, IrType>? {
+  return optionalKind(context, declaration)
+}
+
+internal fun IrType.optionalKind(context: IrMetroContext? = null, declaration: IrDeclaration? = null): Pair<OptionalKind, IrType>? {
+  val classId = rawTypeOrNull()?.classId ?: return null
+  val kind = when (classId) {
+    Symbols.ClassIds.JavaOptional -> OptionalKind.JAVA
+    else -> return null
+  }
+  val type = when (val typeArg = requireSimpleType(declaration).arguments[0]) {
+    is IrStarProjection -> {
+      val message = "Optional type argument is star projection"
+      context?.reportCompat(declaration, MetroDiagnostics.METRO_ERROR, message) ?: error(message)
+      return null
+    }
+    is IrTypeProjection -> typeArg.type
+  }
+  return kind to type
+}
+
+internal enum class OptionalKind(val classId: ClassId) {
+  JAVA(Symbols.ClassIds.JavaOptional),
+  // Other types would go here
 }
