9655a7d6e1bb3f090e06567a3b92f72715020ed1
Zac Sweers <pandanomic@gmail.com>
2025-10-09 17:34:38 -0400

    Add OptionalDependencyBehavior

---
+++ gradle-plugin/src/main/kotlin/dev/zacsweers/metro/gradle/OptionalDependencyBehavior.kt
@@ -0,0 +1,25 @@
+package dev.zacsweers.metro.gradle
+
+/** Controls the behavior of optional dependencies on a per-graph basis. */
+public enum class OptionalDependencyBehavior {
+  /**
+   * Disable all optional dependencies. Default values are allowed on parameters but ignored by the
+   * compiler.
+   */
+  DISABLE,
+
+  /**
+   * In this mode, the presence of a default value on a parameter alone indicates that dependency is
+   * optional.
+   *
+   * Note that for optional graph accessors, `@OptionalDependency` is still necessary.
+   */
+  DEFAULT,
+
+  /**
+   * In this mode, all optional dependencies must by annotated with `@OptionalDependency` (in both
+   * graph accessors as well as injected parameters). This can be desirable for consistency with
+   * accessors and/or to otherwise make the behavior more explicit.
+   */
+  REQUIRE_OPTIONAL_DEPENDENCY,
+}
