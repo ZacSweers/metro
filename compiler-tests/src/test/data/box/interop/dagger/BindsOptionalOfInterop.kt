af71e16f318d5240cfacef5eb0aa5f4165b65eef
Zac Sweers <pandanomic@gmail.com>
2025-10-09 14:49:38 -0400

    Enable rest of the test

--- compiler-tests/src/test/data/box/interop/dagger/BindsOptionalOfInterop.kt
+++ compiler-tests/src/test/data/box/interop/dagger/BindsOptionalOfInterop.kt
@@ -20,19 +20,19 @@ interface Bindings {
 interface ExampleGraph {
   val emptyOptionalAccessor: Optional<String>
   val presentOptionalAccessor: Optional<Int>
-//  val stringConsumer: StringConsumer
+  val stringConsumer: StringConsumer
 
   @Provides fun provideInt(): Int = 3
 }
 
-//class StringConsumer @Inject constructor(
-//  val value: Optional<String>
-//)
+class StringConsumer @Inject constructor(
+  val value: Optional<String>
+)
 
 fun box(): String {
   val graph = createGraph<ExampleGraph>()
   assertTrue(graph.emptyOptionalAccessor.isEmpty())
   assertEquals(3, graph.presentOptionalAccessor.get())
-//  assertTrue(graph.stringConsumer.value.isEmpty())
+  assertTrue(graph.stringConsumer.value.isEmpty())
   return "OK"
 }
