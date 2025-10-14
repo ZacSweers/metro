fac649715fc66c4f42e0420db8d671a53e8041da
Zac Sweers <pandanomic@gmail.com>
2025-10-14 17:08:08 -0400

    regen

--- compiler/src/main/kotlin/dev/zacsweers/metro/compiler/ir/transformers/TransformerContextAccess.kt
+++ compiler/src/main/kotlin/dev/zacsweers/metro/compiler/ir/transformers/TransformerContextAccess.kt
@@ -1,3 +1,5 @@
+// Copyright (C) 2025 Zac Sweers
+// SPDX-License-Identifier: Apache-2.0
 package dev.zacsweers.metro.compiler.ir.transformers
 
 import org.jetbrains.kotlin.backend.common.ScopeWithIr
