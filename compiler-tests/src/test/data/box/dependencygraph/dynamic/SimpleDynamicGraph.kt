c1cccab3f40123d59ffb904abe68ae47795dc995
Zac Sweers <pandanomic@gmail.com>
2025-10-14 16:44:21 -0400

    Broaden test

--- compiler-tests/src/test/data/box/dependencygraph/dynamic/SimpleDynamicGraph.kt
+++ compiler-tests/src/test/data/box/dependencygraph/dynamic/SimpleDynamicGraph.kt
@@ -2,21 +2,27 @@
 interface AppGraph {
   val int: Int
 
+  // By default this provides 3
   @Provides fun provideInt(): Int = 3
 }
 
 class Example {
-  fun someTest(): Int {
-    val testGraph = createGraph<AppGraph>(TestIntProvider())
+  fun someTest(value: Int): Int {
+    // Graph in a class
+    val testGraph = createGraph<AppGraph>(TestIntProvider(value))
     return testGraph.int
-  }
-
-  class TestIntProvider {
-    @Provides fun provideInt(): Int = 2
   }
 }
 
+@BindingContainer
+class TestIntProvider(private val value: Int) {
+  @Provides fun provideInt(): Int = value
+}
+
 fun box(): String {
-  assertEquals(2, Example().someTest())
+  assertEquals(2, Example().someTest(2))
+
+  // top-level function-only
+  assertEquals(5, createGraph<AppGraph>(TestIntProvider(5)).int)
   return "OK"
 }
