7ffa5fdce5cd96f1119dae32136345334bf9dd16
Zac Sweers <pandanomic@gmail.com>
2025-10-17 14:04:48 -0400

    Formatting

--- compiler/src/main/kotlin/dev/zacsweers/metro/compiler/ir/GraphPropertyData.kt
+++ compiler/src/main/kotlin/dev/zacsweers/metro/compiler/ir/GraphPropertyData.kt
@@ -1,3 +1,5 @@
+// Copyright (C) 2025 Zac Sweers
+// SPDX-License-Identifier: Apache-2.0
 package dev.zacsweers.metro.compiler.ir
 
 import org.jetbrains.kotlin.ir.builders.declarations.addBackingField
