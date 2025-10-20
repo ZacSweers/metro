cc2d047332284697aaf792b1087f475698b64f06
Zac Sweers <pandanomic@gmail.com>
2025-10-20 16:45:39 -0400

    cleanup

--- compiler-tests/src/test/kotlin/dev/zacsweers/metro/compiler/compilerVersions.kt
+++ compiler-tests/src/test/kotlin/dev/zacsweers/metro/compiler/compilerVersions.kt
@@ -11,10 +11,11 @@ fun targetKotlinVersionString(testServices: TestServices): String? {
 
 fun targetKotlinVersion(testServices: TestServices): KotlinVersion? {
   val versionString = targetKotlinVersionString(testServices) ?: return null
+  return KotlinVersion.parse(versionString)
+}
 
-  val targetKotlinVersion =
-    versionString.substringBefore('-').split('.').let { (major, minor, patch) ->
-      KotlinVersion(major.toInt(), minor.toInt(), patch.toInt())
-    }
-  return targetKotlinVersion
+fun KotlinVersion.Companion.parse(versionString: String): KotlinVersion {
+  return versionString.substringBefore('-').split('.').let { (major, minor, patch) ->
+    KotlinVersion(major.toInt(), minor.toInt(), patch.toInt())
+  }
 }
