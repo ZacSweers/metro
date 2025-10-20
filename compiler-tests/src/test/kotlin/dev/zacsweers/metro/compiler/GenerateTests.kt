0ec94db2b2bc6f0a40aad68332b76b61c3d810d7
Zac Sweers <pandanomic@gmail.com>
2025-10-20 16:34:09 -0400

    Exclude by targets

--- compiler-tests/src/test/kotlin/dev/zacsweers/metro/compiler/GenerateTests.kt
+++ compiler-tests/src/test/kotlin/dev/zacsweers/metro/compiler/GenerateTests.kt
@@ -2,6 +2,18 @@
 // SPDX-License-Identifier: Apache-2.0
 package dev.zacsweers.metro.compiler
 
+import dev.zacsweers.metro.compiler.test.COMPILER_VERSION
+import org.intellij.lang.annotations.Language
+
 fun main() {
-  generateTests<AbstractBoxTest, AbstractDiagnosticTest, AbstractFirDumpTest, AbstractIrDumpTest>()
+  val targetCompilerVersion = COMPILER_VERSION
+  val versionString = targetCompilerVersion.toString().filterNot { it == '.' }
+
+  // Exclude files with .k<version> where version != targetCompilerVersion
+  // Pattern must match the full filename (with ^ and $ anchors)
+  @Language("RegExp")
+  val exclusionPattern = """^(.+)\.k(?!$versionString\b)\w+\.kts?$"""
+  generateTests<AbstractBoxTest, AbstractDiagnosticTest, AbstractFirDumpTest, AbstractIrDumpTest>(
+    exclusionPattern
+  )
 }
