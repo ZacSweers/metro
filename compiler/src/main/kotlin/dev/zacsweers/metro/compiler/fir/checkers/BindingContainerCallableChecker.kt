dbe7ae579c5b00ff37675cc77650b54740b6a834
Zac Sweers <pandanomic@gmail.com>
2025-11-06 11:26:34 -0500

    Update FIR usages

--- compiler/src/main/kotlin/dev/zacsweers/metro/compiler/fir/checkers/BindingContainerCallableChecker.kt
+++ compiler/src/main/kotlin/dev/zacsweers/metro/compiler/fir/checkers/BindingContainerCallableChecker.kt
@@ -13,6 +13,7 @@ import dev.zacsweers.metro.compiler.fir.classIds
 import dev.zacsweers.metro.compiler.fir.compatContext
 import dev.zacsweers.metro.compiler.fir.findInjectConstructors
 import dev.zacsweers.metro.compiler.fir.isAnnotatedWithAny
+import dev.zacsweers.metro.compiler.fir.isBindingContainer
 import dev.zacsweers.metro.compiler.fir.metroFirBuiltIns
 import dev.zacsweers.metro.compiler.fir.scopeAnnotations
 import dev.zacsweers.metro.compiler.fir.validateInjectionSiteType
@@ -71,8 +72,7 @@ internal object BindingContainerCallableChecker :
     }
     if (declaration is FirConstructor) {
       val isInBindingContainer =
-        containingClassSymbol?.isAnnotatedWithAny(session, classIds.bindingContainerAnnotations)
-          ?: false
+        containingClassSymbol?.isBindingContainer(session) ?: false
       if (isInBindingContainer) {
         // Check for Provides annotations on constructor params
         for (param in declaration.valueParameters) {
@@ -183,7 +183,7 @@ internal object BindingContainerCallableChecker :
 
     if (annotations.isProvides) {
       containingClassSymbol?.let { containingClass ->
-        if (!containingClass.isAnnotatedWithAny(session, classIds.bindingContainerAnnotations)) {
+        if (!containingClass.isBindingContainer(session)) {
           if (containingClass.classKind?.isObject == true && !containingClass.isCompanion) {
             // @Provides declarations can't live in non-@BindingContainer objects, this is a common
             // case hit when migrating from Dagger/Anvil and you have a non-contributed @Module,
