57ab43b79de9b9cfaffe0226871d0f8e831874ad
Zac Sweers <pandanomic@gmail.com>
2025-11-30 16:32:47 -0500

    (pick) Use convention defaults

---
+++ gradle-plugin/src/main/kotlin/dev/zacsweers/metro/gradle/KotlinVersions.kt
@@ -0,0 +1,8 @@
+package dev.zacsweers.metro.gradle
+
+import org.jetbrains.kotlin.tooling.core.KotlinToolingVersion
+
+internal object KotlinVersions {
+  val kotlin230 = KotlinToolingVersion(2, 3, 0, null)
+  val kotlin2320 = KotlinToolingVersion(2, 3, 20, null)
+}
