fff0c00be899b1e9452b1c9ce60f8c7391cf34f4
Zac Sweers <pandanomic@gmail.com>
2025-12-01 00:32:38 -0500

    Add dev and IJ deps repos to functional tests

--- gradle-plugin/src/functionalTest/kotlin/dev/zacsweers/metro/gradle/MetroProject.kt
+++ gradle-plugin/src/functionalTest/kotlin/dev/zacsweers/metro/gradle/MetroProject.kt
@@ -7,6 +7,10 @@ import com.autonomousapps.kit.GradleProject
 import com.autonomousapps.kit.GradleProject.DslKind
 import com.autonomousapps.kit.Source
 import com.autonomousapps.kit.gradle.BuildScript
+import com.autonomousapps.kit.gradle.DependencyResolutionManagement
+import com.autonomousapps.kit.gradle.PluginManagement
+import com.autonomousapps.kit.gradle.Repositories
+import com.autonomousapps.kit.gradle.Repository
 
 abstract class MetroProject(
   private val debug: Boolean = false,
@@ -24,8 +28,22 @@ abstract class MetroProject(
         .withRootProject {
           sources = this@MetroProject.sources()
           withBuildScript { applyMetroDefault() }
+          withSettingsScript {
+            pluginManagement = PluginManagement(metroRepositories(Repository.DEFAULT_PLUGINS))
+            dependencyResolutionManagement =
+              DependencyResolutionManagement(metroRepositories(Repository.DEFAULT))
+          }
         }
         .write()
+
+  private fun metroRepositories(defaults: List<Repository>): Repositories =
+    Repositories(
+      mutableListOf<Repository>().apply {
+        addAll(defaults)
+        add(Repository.ofMaven("https://packages.jetbrains.team/maven/p/kt/dev/"))
+        add(Repository.ofMaven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies/"))
+      }
+    )
 
   /** Generates just the `metro { ... }` block content for use in custom build scripts. */
   fun buildMetroBlock(): String = buildString {
