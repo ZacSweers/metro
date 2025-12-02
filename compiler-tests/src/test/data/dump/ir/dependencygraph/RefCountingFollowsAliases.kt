c5acd00756234a55e9b7b9403d9b229e5b7a8a6d
Zac Sweers <pandanomic@gmail.com>
2025-12-02 16:32:06 -0500

    Multi-level test

--- compiler-tests/src/test/data/dump/ir/dependencygraph/RefCountingFollowsAliases.kt
+++ compiler-tests/src/test/data/dump/ir/dependencygraph/RefCountingFollowsAliases.kt
@@ -1,5 +1,7 @@
 // https://github.com/ZacSweers/metro/issues/1462
 // GENERATE_CONTRIBUTION_HINTS: false
+
+// Single-level alias: Foo -> FooImpl
 interface Foo
 
 @Inject
@@ -12,11 +14,28 @@ class BarA(val foo: Foo)
 @Inject
 class BarB(val foo: Foo)
 
+// Multi-level alias chain: Baz -> BazMiddle -> BazImpl
+interface Baz
+
+interface BazMiddle : Baz
+
+@Inject
+@ContributesBinding(AppScope::class)
+class BazImpl : BazMiddle
+
+@Inject
+class QuxA(val baz: Baz)
+
+@Inject
+class QuxB(val baz: Baz)
+
 @Inject
 @SingleIn(AppScope::class)
-class Main(val barA: BarA, val barB: BarB)
+class Main(val barA: BarA, val barB: BarB, val quxA: QuxA, val quxB: QuxB)
 
 @DependencyGraph(AppScope::class)
 interface AppGraph {
   val main: Main
+
+  @Binds fun bindBaz(impl: BazMiddle): Baz
 }
