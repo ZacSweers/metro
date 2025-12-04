d4e50fa922f9b74c2d5206306bc13cd7d3ef2042
Bridget Phillips <wphillips@squareup.com>
2025-12-04 13:33:43 -0800

    Add usage information to AppScope's javadoc

--- runtime/src/commonMain/kotlin/dev/zacsweers/metro/AppScope.kt
+++ runtime/src/commonMain/kotlin/dev/zacsweers/metro/AppScope.kt
@@ -2,5 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 package dev.zacsweers.metro
 
-/** A simple common app-wide _scope key_ that can be used with [SingleIn]. */
+/** 
+ * A simple common app-wide _scope key_ that can be used with [SingleIn]. 
+ *
+ * AppScope must be configured by hand within your own dependency graph to be used -
+ * Metro does not perform any automatic scope configuration.
+ */
 public abstract class AppScope private constructor()
