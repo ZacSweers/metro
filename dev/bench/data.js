02bc3b74effa373b0f0d524224e46d0032dd89c7
github-action-benchmark <github@users.noreply.github.com>
2025-12-20 05:39:36 +0000

    add Startup (current) (jmh) benchmark result for 700339fda1f58ef172f314b639e42c0a3149e4ce

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1766209174548,
+  "lastUpdate": 1766209175724,
   "repoUrl": "https://github.com/ZacSweers/metro",
   "entries": {
     "Startup (baseline)": [
@@ -28,6 +28,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "baseline",
             "value": 0.2208381201865796,
+            "unit": "ms/op",
+            "extra": "iterations: 10\nforks: 2\nthreads: 1"
+          }
+        ]
+      }
+    ],
+    "Startup (current)": [
+      {
+        "commit": {
+          "author": {
+            "email": "pandanomic@gmail.com",
+            "name": "Zac Sweers",
+            "username": "ZacSweers"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "700339fda1f58ef172f314b639e42c0a3149e4ce",
+          "message": "Add double-graph-like annotation diagnostic (#1575)",
+          "timestamp": "2025-12-20T04:52:34Z",
+          "tree_id": "7696bdbe571d483a8ed42a6235ec5824f7505503",
+          "url": "https://github.com/ZacSweers/metro/commit/700339fda1f58ef172f314b639e42c0a3149e4ce"
+        },
+        "date": 1766209175419,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "current",
+            "value": 0.21224508689795174,
             "unit": "ms/op",
             "extra": "iterations: 10\nforks: 2\nthreads: 1"
           }
