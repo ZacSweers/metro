5f1f88be911f62645c031e725151f81f58fd66c8
github-action-benchmark <github@users.noreply.github.com>
2025-12-20 06:42:26 +0000

    add Startup (current) (jmh) benchmark result for dd3cdf9fc73d5bea12d38406ff7f3c3cb1a2599d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1766212944829,
+  "lastUpdate": 1766212945759,
   "repoUrl": "https://github.com/ZacSweers/metro",
   "entries": {
     "Startup (baseline)": [
@@ -90,6 +90,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "current",
             "value": 0.21224508689795174,
+            "unit": "ms/op",
+            "extra": "iterations: 10\nforks: 2\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pandanomic@gmail.com",
+            "name": "Zac Sweers",
+            "username": "ZacSweers"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "dd3cdf9fc73d5bea12d38406ff7f3c3cb1a2599d",
+          "message": "Simplify scenarios, add noop/vanilla, cleanups (#1576)",
+          "timestamp": "2025-12-20T05:56:08Z",
+          "tree_id": "fe0ec9b5427f8853a16d6f6cacaf36544af4f01b",
+          "url": "https://github.com/ZacSweers/metro/commit/dd3cdf9fc73d5bea12d38406ff7f3c3cb1a2599d"
+        },
+        "date": 1766212945361,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "current",
+            "value": 0.22516799708731344,
             "unit": "ms/op",
             "extra": "iterations: 10\nforks: 2\nthreads: 1"
           }
