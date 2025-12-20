b179c56a8019c34e214a6f234a09e5b21d4a930b
github-action-benchmark <github@users.noreply.github.com>
2025-12-20 05:39:37 +0000

    add Build Time (baseline) (customSmallerIsBetter) benchmark result for 700339fda1f58ef172f314b639e42c0a3149e4ce

---
+++ dev/bench/build/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1766209176688,
+  "repoUrl": "https://github.com/ZacSweers/metro",
+  "entries": {
+    "Build Time (baseline)": [
+      {
+        "commit": {
+          "author": {
+            "email": "pandanomic@gmail.com",
+            "name": "Zac Sweers",
+            "username": "ZacSweers"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "700339fda1f58ef172f314b639e42c0a3149e4ce",
+          "message": "Add double-graph-like annotation diagnostic (#1575)",
+          "timestamp": "2025-12-20T04:52:34Z",
+          "tree_id": "7696bdbe571d483a8ed42a6235ec5824f7505503",
+          "url": "https://github.com/ZacSweers/metro/commit/700339fda1f58ef172f314b639e42c0a3149e4ce"
+        },
+        "date": 1766209176384,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Build Time (baseline)",
+            "value": 17333.696,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
