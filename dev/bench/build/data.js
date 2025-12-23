171e3e14196850a166df22b1ceebd83c00e41dee
github-action-benchmark <github@users.noreply.github.com>
2025-12-23 02:41:49 +0000

    add Build Time (baseline) (customSmallerIsBetter) benchmark result for f7facf8a2a2cc119333ebefb548728736c5a9472

--- dev/bench/build/data.js
+++ dev/bench/build/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1766212948027,
+  "lastUpdate": 1766457708872,
   "repoUrl": "https://github.com/ZacSweers/metro",
   "entries": {
     "Build Time (baseline)": [
@@ -57,6 +57,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Build Time (baseline)",
             "value": 17154.59,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "6785522+JoelWilcox@users.noreply.github.com",
+            "name": "Joel Wilcox",
+            "username": "JoelWilcox"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f7facf8a2a2cc119333ebefb548728736c5a9472",
+          "message": "Fix rank-based binding replacements getting dropped for multi-contribution classes in root graphs (#1583)",
+          "timestamp": "2025-12-22T20:40:51-05:00",
+          "tree_id": "d71d835af62a9fbc6fe7efa301b99a18c6bda77e",
+          "url": "https://github.com/ZacSweers/metro/commit/f7facf8a2a2cc119333ebefb548728736c5a9472"
+        },
+        "date": 1766457708473,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Build Time (baseline)",
+            "value": 19617.438000000002,
             "unit": "ms"
           }
         ]
