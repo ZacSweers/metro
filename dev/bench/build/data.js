e09d36873493fd4a8bea5e6105601b7fb690ecd9
github-action-benchmark <github@users.noreply.github.com>
2025-12-23 05:46:37 +0000

    add Build Time (baseline) (customSmallerIsBetter) benchmark result for 535463d0bf8fae52826f81c8c34b5bbb3b43b2f8

--- dev/bench/build/data.js
+++ dev/bench/build/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1766457709822,
+  "lastUpdate": 1766468796867,
   "repoUrl": "https://github.com/ZacSweers/metro",
   "entries": {
     "Build Time (baseline)": [
@@ -86,6 +86,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Build Time (baseline)",
             "value": 19617.438000000002,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pandanomic@gmail.com",
+            "name": "Zac Sweers",
+            "username": "ZacSweers"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "535463d0bf8fae52826f81c8c34b5bbb3b43b2f8",
+          "message": "More benchmark work (#1581)",
+          "timestamp": "2025-12-22T23:59:38-05:00",
+          "tree_id": "9b9a9e58afad23d3be528a522e6743ed3d7d3af1",
+          "url": "https://github.com/ZacSweers/metro/commit/535463d0bf8fae52826f81c8c34b5bbb3b43b2f8"
+        },
+        "date": 1766468796465,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Build Time (baseline)",
+            "value": 17987.057999999997,
             "unit": "ms"
           }
         ]
