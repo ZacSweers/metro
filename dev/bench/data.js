694c011f1b6aef13b32566abb1772b2af0c6152f
github-action-benchmark <github@users.noreply.github.com>
2025-12-23 05:46:36 +0000

    add Startup (current) (jmh) benchmark result for 535463d0bf8fae52826f81c8c34b5bbb3b43b2f8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1766468794816,
+  "lastUpdate": 1766468795742,
   "repoUrl": "https://github.com/ZacSweers/metro",
   "entries": {
     "Startup (baseline)": [
@@ -210,6 +210,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "current",
             "value": 0.21884228058965824,
+            "unit": "ms/op",
+            "extra": "iterations: 10\nforks: 2\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pandanomic@gmail.com",
+            "name": "Zac Sweers",
+            "username": "ZacSweers"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "535463d0bf8fae52826f81c8c34b5bbb3b43b2f8",
+          "message": "More benchmark work (#1581)",
+          "timestamp": "2025-12-22T23:59:38-05:00",
+          "tree_id": "9b9a9e58afad23d3be528a522e6743ed3d7d3af1",
+          "url": "https://github.com/ZacSweers/metro/commit/535463d0bf8fae52826f81c8c34b5bbb3b43b2f8"
+        },
+        "date": 1766468795343,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "current",
+            "value": 0.21719329423978614,
             "unit": "ms/op",
             "extra": "iterations: 10\nforks: 2\nthreads: 1"
           }
