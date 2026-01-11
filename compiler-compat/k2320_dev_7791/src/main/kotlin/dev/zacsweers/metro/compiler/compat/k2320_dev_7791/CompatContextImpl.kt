b1b4131a3485ee84084f0fda753a09b21646dade
Zac Sweers <pandanomic@gmail.com>
2026-01-11 01:02:36 -0500

    Update to 7791

--- compiler-compat/k2320_dev_7791/src/main/kotlin/dev/zacsweers/metro/compiler/compat/k2320_dev_7791/CompatContextImpl.kt
+++ compiler-compat/k2320_dev_7791/src/main/kotlin/dev/zacsweers/metro/compiler/compat/k2320_dev_7791/CompatContextImpl.kt
@@ -1,6 +1,6 @@
 // Copyright (C) 2026 Zac Sweers
 // SPDX-License-Identifier: Apache-2.0
-package dev.zacsweers.metro.compiler.compat.k2320_dev_7778
+package dev.zacsweers.metro.compiler.compat.k2320_dev_7791
 
 import dev.zacsweers.metro.compiler.compat.CompatContext
 import dev.zacsweers.metro.compiler.compat.k2320_dev_5437.CompatContextImpl as DelegateType
@@ -42,7 +42,7 @@ public class CompatContextImpl : CompatContext by DelegateType() {
   }
 
   public class Factory : CompatContext.Factory {
-    override val minVersion: String = "2.3.20-dev-7778"
+    override val minVersion: String = "2.3.20-dev-7791"
 
     override fun create(): CompatContext = CompatContextImpl()
   }
