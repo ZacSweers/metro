06aed16656a8cc85da587d3ec8da376aa7e46d29
Zac Sweers <pandanomic@gmail.com>
2026-01-17 18:23:25 -0500

    Update test

--- compiler/src/test/kotlin/dev/zacsweers/metro/compiler/transformers/BindingContainerTransformerTest.kt
+++ compiler/src/test/kotlin/dev/zacsweers/metro/compiler/transformers/BindingContainerTransformerTest.kt
@@ -617,7 +617,11 @@ class BindingContainerTransformerTest : MetroCompilerTest() {
     ) {
       ExampleGraph.generatedImpl().createGraphWithNoArgs()
       assertDiagnostics(
-        "w: Included Dagger module 'ExampleModule' declares a `subcomponents` parameter but this will be ignored by Metro in interop."
+        """
+        w: Included Dagger module 'ExampleModule' declares a `subcomponents` parameter but this will be ignored by Metro in interop.
+        w: ExampleGraph.kt:6:39 Graph input 'ExampleModule' is unused and can be removed.
+        """
+          .trimIndent()
       )
     }
   }
