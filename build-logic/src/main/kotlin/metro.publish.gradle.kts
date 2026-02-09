3eef416231ad50349f0bd4d2948c1e540a57b5b2
Zac Sweers <pandanomic@gmail.com>
2026-02-09 15:46:28 -0500

    Fix publish plugin missing

--- build-logic/src/main/kotlin/metro.publish.gradle.kts
+++ build-logic/src/main/kotlin/metro.publish.gradle.kts
@@ -1,6 +1,8 @@
 // Copyright (C) 2026 Zac Sweers
 // SPDX-License-Identifier: Apache-2.0
 
+apply(plugin = "com.vanniktech.maven.publish")
+
 project.extensions.create<MetroPublishExtension>("metroPublish").apply {
   artifactId.convention(project.name)
 }
