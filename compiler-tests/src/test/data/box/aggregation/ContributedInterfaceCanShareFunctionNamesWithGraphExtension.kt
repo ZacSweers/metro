0f99da914dc3d0237035d9bf322e5a99bdafe74f
Marcin Robaczyński <marcin.robaczynski@whatnot.com>
2026-02-12 16:45:54 +0100

    Add test case for name clashing issue where scopes are not respected

---
+++ compiler-tests/src/test/data/box/aggregation/ContributedInterfaceCanShareFunctionNamesWithGraphExtension.kt
@@ -0,0 +1,54 @@
+data class Dependency(val name: String)
+
+@DependencyGraph(scope = AppScope::class)
+interface AppGraph {
+  val contributedInterface: ContributedInterface
+}
+
+@MyScope
+@GraphExtension(scope = MyScope::class)
+interface MyGraph {
+
+  @Named("sameFunctionName")
+  fun dependency(): Dependency
+
+  @Named("otherFunctionName")
+  // this has a different name than the one from MyModule
+  fun otherDependency(): Dependency
+
+  @ContributesTo(AppScope::class)
+  @GraphExtension.Factory
+  interface Factory {
+    fun createMyGraph(): MyGraph
+  }
+}
+@ContributesTo(MyScope::class)
+interface MyModule {
+  @Provides
+  @MyScope
+  @Named("sameFunctionName")
+  fun dependency(): Dependency = Dependency("regular")
+
+  @Provides
+  @MyScope
+  @Named("otherFunctionName")
+  fun dependency2(): Dependency = Dependency("regular")
+}
+
+
+fun box(): String {
+
+  val myGraph = createGraph<AppGraph>().createMyGraph()
+  val firstNonClashing = myGraph.otherCoroutineScope()
+  val secondNonClashing = myGraph.otherCoroutineScope()
+  val firstClashing = myGraph.coroutineScope()
+  val secondClashing = myGraph.coroutineScope()
+
+  assertEquals(firstNonClashing, secondNonClashing)
+  assertTrue(firstNonClashing === secondNonClashing)
+
+  assertEquals(firstClashing, secondClashing)
+  assertTrue(firstClashing === secondClashing)
+
+  return "OK"
+}
