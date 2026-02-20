2b6327430c8717de0f00612271aea683a192f61b
Joel Wilcox <jwilcox@squareup.com>
2026-02-19 16:53:41 -0800

    Reduce DeeplyNestedGraphsKeysPopulatedReport test graph depth for Windows integration test compatibility
    - Coverage-wise, this is fine because we still have a box test for the deeply nested graph use-case.

--- compiler-tests/src/test/data/dump/reports/DeeplyNestedGraphsKeysPopulatedReport.kt
+++ compiler-tests/src/test/data/dump/reports/DeeplyNestedGraphsKeysPopulatedReport.kt
@@ -3,34 +3,19 @@
 // exceptions for the report files. We are not able to test the same behavior here because defining
 // a sufficiently nested graph in a dump test results in a 'file name too long' exception for an
 // equivalent '.class' file reference in the generated Java [ReportsTestGenerated] file.
+//
+// The graph depth is further limited here due to limitation when running the integration tests on
+// windows due to a limit on the full path length, rather than just name lengths.
 
 // CHECK_REPORTS: keys-populated/ProductDevAppDependencyGraph/Impl
 // CHECK_REPORTS: keys-populated/ProductDevAppDependencyGraph/Impl/ProductDevLoggedInComponentImpl
 // CHECK_REPORTS: keys-populated/ProductDevAppDependencyGraph/Impl/ProductDevLoggedInComponentImpl/ProductDevMainActivityComponentImpl
-// CHECK_REPORTS: keys-populated/ProductDevAppDependencyGraph/Impl/ProductDevLoggedInComponentImpl/ProductDevMainActivityComponentImpl/EditEstimateConfigureItemScopeComponentImpl
-// CHECK_REPORTS: keys-populated/ProductDevAppDependencyGraph/Impl/ProductDevLoggedInComponentImpl/ProductDevMainActivityComponentImpl/EditEstimateConfigureItemScopeComponentImpl/ConfigureItemComponentImpl
-// CHECK_REPORTS: keys-populated/ProductDevAppDependencyGraph/Impl/ProductDevLoggedInComponentImpl/ProductDevMainActivityComponentImpl/EditEstimateConfigureItemScopeComponentImpl/ConfigureItemComponentImpl/ProductConfigureItemDetailScreenComponentImpl
 
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.SingleIn
 
 @GraphExtension
-interface ProductConfigureItemDetailScreenComponent
-
-@GraphExtension
-interface ConfigureItemComponent {
-  val child: ProductConfigureItemDetailScreenComponent
-}
-
-@GraphExtension
-interface EditEstimateConfigureItemScopeComponent {
-  val child: ConfigureItemComponent
-}
-
-@GraphExtension
-interface ProductDevMainActivityComponent {
-  val child: EditEstimateConfigureItemScopeComponent
-}
+interface ProductDevMainActivityComponent
 
 @GraphExtension
 interface ProductDevLoggedInComponent {
