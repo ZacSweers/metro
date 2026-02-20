c0b342b2617383b4ab939b268b2a85b0362d4116
Jacob Applin <japplin@squareup.com>
2026-02-20 09:31:47 -0500

    Failing test reproducer for parallel threads

--- compiler-tests/src/test/java/dev/zacsweers/metro/compiler/BoxTestGenerated.java
+++ compiler-tests/src/test/java/dev/zacsweers/metro/compiler/BoxTestGenerated.java
@@ -1182,6 +1182,12 @@ public class BoxTestGenerated extends AbstractBoxTest {
       }
 
       @Test
+      @TestMetadata("DaggerSubcomponentScopeInheritanceWithParallelThreads.kt")
+      public void testDaggerSubcomponentScopeInheritanceWithParallelThreads() {
+        runTest("compiler-tests/src/test/data/box/dependencygraph/parallel/DaggerSubcomponentScopeInheritanceWithParallelThreads.kt");
+      }
+
+      @Test
       @TestMetadata("DeepHierarchyWithParallelValidationStressTest.kt")
       public void testDeepHierarchyWithParallelValidationStressTest() {
         runTest("compiler-tests/src/test/data/box/dependencygraph/parallel/DeepHierarchyWithParallelValidationStressTest.kt");
