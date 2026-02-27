9fd49249c3e9f166b9d675638655101d233cf69e
Zac Sweers <pandanomic@gmail.com>
2026-02-27 16:35:57 -0500

    Significantly simplify

--- compiler-tests/src/test/data/box/contributesgraphextension/ContributesSingletonGraphIntoGraphExtension.kt
+++ compiler-tests/src/test/data/box/contributesgraphextension/ContributesSingletonGraphIntoGraphExtension.kt
@@ -1,47 +1,32 @@
-// MODULE: user
-@DependencyGraph
-interface LoggedInGraph {
-    fun getUser(): String
-
-    @DependencyGraph.Factory
-    interface Factory {
-        fun create(@Provides user: String): LoggedInGraph
-    }
+interface UserProvider {
+  fun getUser(): String
 }
 
-// MODULE: example(user)
-@SingleIn(AppScope::class)
 @DependencyGraph(AppScope::class)
 interface ExampleGraph {
-    fun child(): ExampleChildGraph.Factory
+  fun child(): ExampleChildGraph
 
-    @DependencyGraph.Factory
-    interface Factory {
-        fun create(@Includes loggedInGraph: LoggedInGraph): ExampleGraph
-    }
+  @DependencyGraph.Factory
+  interface Factory {
+    fun create(@Includes userProvider: UserProvider): ExampleGraph
+  }
 }
 
 @GraphExtension
 interface ExampleChildGraph {
-    fun repository(): ExampleRepository
-
-    @GraphExtension.Factory
-    interface Factory {
-        fun create(): ExampleChildGraph
-    }
+  fun repository(): ExampleRepository
 }
 
-@Inject
-class ExampleUseCase(private val loggedInGraph: LoggedInGraph)
+@Inject @SingleIn(AppScope::class) class ExampleRepository(private val userProvider: UserProvider)
 
-@Inject
-@SingleIn(AppScope::class)
-class ExampleRepository(val useCase: ExampleUseCase)
-
-// MODULE: main(user, example)
 fun box(): String {
-    val loggedInGraph = createGraphFactory<LoggedInGraph.Factory>().create("user")
-    val exampleGraph = createGraphFactory<ExampleGraph.Factory>().create(loggedInGraph)
-    val repository = exampleGraph.child().create().repository() // ClassCastException
-    return "OK"
+  val userProvider =
+    object : UserProvider {
+      override fun getUser(): String {
+        return "user"
+      }
+    }
+  val exampleGraph = createGraphFactory<ExampleGraph.Factory>().create(userProvider)
+  val repository = exampleGraph.child().repository() // ClassCastException
+  return "OK"
 }
