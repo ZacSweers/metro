ZacSweers/metro

commit 85543469edb769d13099c2b49608b38d8baf2c90

Move class and provides factory function generation back to IR (#1953)

compiler/src/main/kotlin/dev/zacsweers/metro/compiler/ir/transformers/InjectedClassTransformer.kt
@@ -9,21 +9,24 @@ import dev.zacsweers.metro.compiler.generatedClass
 import dev.zacsweers.metro.compiler.ir.ClassFactory
 import dev.zacsweers.metro.compiler.ir.IrMetroContext
 import dev.zacsweers.metro.compiler.ir.IrTypeKey
+import dev.zacsweers.metro.compiler.ir.addBackingFieldTo
 import dev.zacsweers.metro.compiler.ir.assignConstructorParamsToFields
 import dev.zacsweers.metro.compiler.ir.checkMirrorParamMismatches
 import dev.zacsweers.metro.compiler.ir.contextParameters
 import dev.zacsweers.metro.compiler.ir.copyParameterDefaultValues
 import dev.zacsweers.metro.compiler.ir.createAndAddTemporaryVariable
 import dev.zacsweers.metro.compiler.ir.createIrBuilder
+import dev.zacsweers.metro.compiler.ir.deepRemapperFor
 import dev.zacsweers.metro.compiler.ir.dispatchReceiverFor
 import dev.zacsweers.metro.compiler.ir.finalizeFakeOverride
 import dev.zacsweers.metro.compiler.ir.findInjectableConstructor
+import dev.zacsweers.metro.compiler.ir.generateDefaultConstructorBody
 import dev.zacsweers.metro.compiler.ir.irExprBodySafe
 import dev.zacsweers.metro.compiler.ir.irInvoke
+import dev.zacsweers.metro.compiler.ir.isAnnotatedWithAny
 import dev.zacsweers.metro.compiler.ir.isExternalParent
 import dev.zacsweers.metro.compiler.ir.metroAnnotationsOf
 import dev.zacsweers.metro.compiler.ir.metroMetadata
-import dev.zacsweers.metro.compiler.ir.parameters.Parameter
 import dev.zacsweers.metro.compiler.ir.parameters.Parameters
 import dev.zacsweers.metro.compiler.ir.parameters.dedupeParameters
 import dev.zacsweers.metro.compiler.ir.parameters.parameters
@@ -40,6 +43,10 @@ import java.util.Optional
 import java.util.concurrent.ConcurrentHashMap
 import kotlin.jvm.optionals.getOrNull
 import org.jetbrains.kotlin.descriptors.ClassKind
+import org.jetbrains.kotlin.descriptors.DescriptorVisibilities
+import org.jetbrains.kotlin.ir.builders.declarations.addConstructor
+import org.jetbrains.kotlin.ir.builders.declarations.addFunction
+import org.jetbrains.kotlin.ir.builders.declarations.addValueParameter
 import org.jetbrains.kotlin.ir.builders.irBlockBody
 import org.jetbrains.kotlin.ir.builders.irCallConstructor
 import org.jetbrains.kotlin.ir.builders.irGet
@@ -53,13 +60,17 @@ import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
 import org.jetbrains.kotlin.ir.declarations.IrValueParameter
 import org.jetbrains.kotlin.ir.symbols.IrConstructorSymbol
 import org.jetbrains.kotlin.ir.types.defaultType
+import org.jetbrains.kotlin.ir.types.typeWith
+import org.jetbrains.kotlin.ir.types.typeWithParameters
 import org.jetbrains.kotlin.ir.util.callableId
 import org.jetbrains.kotlin.ir.util.classIdOrFail
 import org.jetbrains.kotlin.ir.util.companionObject
+import org.jetbrains.kotlin.ir.util.defaultType
 import org.jetbrains.kotlin.ir.util.file
 import org.jetbrains.kotlin.ir.util.functions
 import org.jetbrains.kotlin.ir.util.getAnnotation
 import org.jetbrains.kotlin.ir.util.getAnnotationStringValue
+import org.jetbrains.kotlin.ir.util.isObject
 import org.jetbrains.kotlin.ir.util.kotlinFqName
 import org.jetbrains.kotlin.ir.util.nestedClasses
 import org.jetbrains.kotlin.ir.util.nonDispatchParameters
@@ -231,6 +242,40 @@ internal class InjectedClassTransformer(
     val memberInjectParameters = injectors.flatMap { it.requiredParametersByClass.values.flatten() }
 
     val constructorParameters = targetConstructor.parameters()
+
+    val isAssistedInject =
+      listOf(declaration, targetConstructor).any {
+        it.isAnnotatedWithAny(metroSymbols.classIds.assistedInjectAnnotations)
+      }
+
+    if (!isAssistedInject) {
+      // Add factory supertype. It won't be visible in metadata but that's ok, we don't need to read
+      // directly since we'll read the mirror function to get the target type
+      factoryCls.superTypes += metroSymbols.metroFactory.typeWith(declaration.defaultType)
+    }
+
+    // Cannot call addFakeOverrides because FIR2IR has already done that, so we need to add the
+    // invoke override directly later
+    val invokeFunction =
+      factoryCls
+        .addFunction(
+          Symbols.StringNames.INVOKE,
+          declaration.defaultType,
+          isFakeOverride = !isAssistedInject,
+        )
+        .apply {
+          isOperator = true
+          if (!isAssistedInject) {
+            overriddenSymbols = listOf(metroSymbols.providerInvoke)
+          } else {
+            // Add assisted params
+            for (param in constructorParameters.allParameters.filter { it.isAssisted }) {
+              addValueParameter(param.name, param.type)
+            }
+          }
+        }
+    metadataDeclarationRegistrar.registerFunctionAsMetadataVisible(invokeFunction)
+
     val allParameters =
       buildList {
           add(constructorParameters)
@@ -244,49 +289,48 @@ internal class InjectedClassTransformer(
     // The FIR side deduplicates by type key, so the factory constructor has fewer
     // parameters when multiple source params share the same type+qualifier.
     val dedupedParameters =
-      if (options.deduplicateInjectedParams) nonAssistedParameters.dedupeParameters()
-      else nonAssistedParameters
-
-    val ctor = factoryCls.primaryConstructor!!
-
-    val constructorParametersToFields = assignConstructorParamsToFields(ctor, factoryCls)
-
-    // Build a mapping from ALL source parameters to fields, handling the fact that
-    // multiple source params with the same type key share the same deduped field.
-    val dedupedParamToField: Map<Parameter, IrField> =
-      constructorParametersToFields.entries.withIndex().associate { (index, pair) ->
-        val (_, field) = pair
-        val sourceParam = dedupedParameters[index]
-        sourceParam to field
+      if (options.deduplicateInjectedParams) {
+        nonAssistedParameters.dedupeParameters()
+      } else {
+        nonAssistedParameters
       }
-    val typeKeyToField: Map<IrTypeKey, IrField> = buildMap {
-      for ((param, field) in dedupedParamToField) {
-        // Params with defaults have their own entries (they're never deduped), so they
-        // don't belong in the type-key map. Only non-default params share fields by type key.
-        if (!param.hasDefault) {
-          putIfAbsent(param.typeKey, field)
-        }
-      }
-    }
-    val sourceParametersToFields: Map<Parameter, IrField> = buildMap {
-      for (param in nonAssistedParameters) {
-        if (param.hasDefault) {
-          // Params with defaults have their own dedicated fields (looked up by identity)
-          dedupedParamToField[param]?.let { put(param, it) }
-        } else {
-          // Non-default params share fields by type key (deduped)
-          typeKeyToField[param.typeKey]?.let { put(param, it) }
-        }
-      }
+
+    val typeKeyToField = mutableMapOf<IrTypeKey, IrField>()
+    val ctor: IrConstructor
+    if (factoryCls.isObject) {
+      // If it's got no parameters we'll generate it in FIR as an object
+      ctor = factoryCls.primaryConstructor!!
+    } else {
+      // Add constructor
+      // Doesn't have to be metadata-visible
+      ctor =
+        factoryCls
+          .addConstructor {
+            visibility = DescriptorVisibilities.PRIVATE
+            isPrimary = true
+          }
+          .apply {
+            val typeRemapper = declaration.deepRemapperFor(factoryCls.defaultType)
+            addParameters(
+              params = dedupedParameters,
+              wrapInProvider = true,
+              typeRemapper = { type -> typeRemapper.remapType(type) },
+            ) { typeKey, irParam ->
+              typeKeyToField[typeKey] = irParam.addBackingFieldTo(factoryCls)
+            }
+            body = generateDefaultConstructorBody()
+          }
     }
 
     val newInstanceFunction =
       generateCreators(
+        declaration,
         factoryCls,
         ctor.symbol,
         targetConstructor.symbol,
         constructorParameters,
         allParameters,
+        isAssistedInject,
       )
 
     /*
@@ -307,15 +351,13 @@ internal class InjectedClassTransformer(
     // Provider<Lazy<T>>
     override fun invoke(): Example<T> = newInstance(ProviderOfLazy.create(valueProvider))
     */
-    val invoke = factoryCls.requireSimpleFunction(Symbols.StringNames.INVOKE)
-
     implementFactoryInvokeOrGetBody(
-      invoke.owner,
+      invokeFunction,
       factoryCls.thisReceiverOrFail,
       newInstanceFunction,
       constructorParameters,
       injectors,
-      sourceParametersToFields,
+      typeKeyToField,
     )
 
     possiblyImplementInvoke(declaration, constructorParameters)
@@ -359,7 +401,7 @@ internal class InjectedClassTransformer(
     newInstanceFunction: IrSimpleFunction,
     constructorParameters: Parameters,
     injectors: List<MembersInjectorTransformer.MemberInjectClass>,
-    parametersToFields: Map<Parameter, IrField>,
+    fields: Map<IrTypeKey, IrField>,
   ) {
     if (invokeFunction.isFakeOverride) {
       invokeFunction.finalizeFakeOverride(thisReceiver)
@@ -384,7 +426,7 @@ internal class InjectedClassTransformer(
                 val providerInstance =
                   irGetField(
                     irGet(invokeFunction.dispatchReceiverParameter!!),
-                    parametersToFields.getValue(constructorParam),
+                    fields.getValue(constructorParam.typeKey),
                   )
                 val contextKey = targetParam.contextualTypeKey
                 typeAsProviderArgument(
@@ -399,7 +441,10 @@ internal class InjectedClassTransformer(
                 functionParamsByName.getValue(targetParam.originalName)
               }
 
-              else -> reportCompilerBug("Unmatched top level injected function param: $targetParam")
+              else ->
+                reportCompilerBug(
+                  "Unmatched top level injected function param: $targetParam. Available: ${functionParamsByName.keys}"
+                )
             }
           }
 
@@ -436,7 +481,7 @@ internal class InjectedClassTransformer(
                       parametersAsProviderArguments(
                         parameters,
                         invokeFunction.dispatchReceiverParameter!!,
-                        parametersToFields,
+                        fields,
                       )
                     )
                   },
@@ -495,7 +540,7 @@ internal class InjectedClassTransformer(
                     .filter { it.isAssisted }
                     .map { it.asValueParameter },
                 targetParameters = invokeFunction.nonDispatchParameters,
-                targetGraphParameter = null,
+                containerParameter = null,
                 wrapInProvider = false,
                 isTopLevelFunction = true,
               )
@@ -591,11 +636,13 @@ internal class InjectedClassTransformer(
   }
 
   private fun generateCreators(
+    targetClass: IrClass,
     factoryCls: IrClass,
     factoryConstructor: IrConstructorSymbol,
     targetConstructor: IrConstructorSymbol,
     constructorParameters: Parameters,
     allParameters: List<Parameters>,
+    isAssistedInject: Boolean,
   ): IrSimpleFunction {
     // If this is an object, we can generate directly into this object
     val isObject = factoryCls.kind == ClassKind.OBJECT
@@ -619,13 +666,17 @@ internal class InjectedClassTransformer(
       )
 
     // Generate create()
-    @Suppress("RETURN_VALUE_NOT_USED")
     generateStaticCreateFunction(
-      parentClass = classToGenerateCreatorsIn,
-      targetClass = factoryCls,
+      objectClassToGenerateIn = classToGenerateCreatorsIn,
+      factoryClass = factoryCls,
+      sourceTypeParameters = targetClass,
+      returnTypeProvider = { typeParams ->
+        metroSymbols.metroFactory.typeWith(targetClass.symbol.typeWithParameters(typeParams))
+      },
       targetConstructor = factoryConstructor,
       parameters = dedupedMerged,
-      providerFunction = null,
+      isAssistedInject = isAssistedInject,
+      sourceFunction = null,
     )
 
     // newInstance() preserves the original constructor signature (no deduplication)
@@ -633,6 +684,8 @@ internal class InjectedClassTransformer(
     val newInstanceFunction =
       generateStaticNewInstanceFunction(
         parentClass = classToGenerateCreatorsIn,
+        sourceTypeParameters = targetClass,
+        returnTypeProvider = { typeParams -> targetClass.symbol.typeWithParameters(typeParams) },
         sourceMetroParameters = constructorParameters,
         sourceParameters = constructorParameters.regularParameters.map { it.asValueParameter },
       ) { function ->

compiler/src/main/kotlin/dev/zacsweers/metro/compiler/fir/MetroFirExtensionRegistrar.kt
@@ -20,7 +20,6 @@ import dev.zacsweers.metro.compiler.fir.generators.InjectedClassFirGenerator
 import dev.zacsweers.metro.compiler.fir.generators.LoggingFirDeclarationGenerationExtension
 import dev.zacsweers.metro.compiler.fir.generators.LoggingFirSupertypeGenerationExtension
 import dev.zacsweers.metro.compiler.fir.generators.ProvidesFactoryFirGenerator
-import dev.zacsweers.metro.compiler.fir.generators.ProvidesFactorySupertypeGenerator
 import dev.zacsweers.metro.compiler.fir.generators.kotlinOnly
 import java.util.ServiceLoader
 import kotlin.io.path.appendText
@@ -62,11 +61,6 @@ public class MetroFirExtensionRegistrar(
 
     // These are types
     if (!isIde) {
-      +supertypeGenerator(
-        "Supertypes - provider factories",
-        ::ProvidesFactorySupertypeGenerator,
-        false,
-      )
       +{ session: FirSession -> FirAccessorOverrideStatusTransformer(session, compatContext) }
     }
     if (options.transformProvidersToPrivate) {

compiler/src/main/kotlin/dev/zacsweers/metro/compiler/fir/generators/InjectedClassFirGenerator.kt
@@ -74,7 +74,6 @@ import org.jetbrains.kotlin.fir.symbols.impl.FirConstructorSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirFieldSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirNamedFunctionSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirPropertySymbol
-import org.jetbrains.kotlin.fir.symbols.impl.FirTypeParameterSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirValueParameterSymbol
 import org.jetbrains.kotlin.fir.toFirResolvedTypeRef
 import org.jetbrains.kotlin.fir.types.FirResolvedTypeRef
@@ -443,7 +442,7 @@ internal class InjectedClassFirGenerator(session: FirSession, compatContext: Com
         membersInjectorClassIdsToInjectedClass[classId] = injectedClass
         classesToGenerate += classId.shortClassName
       }
-      return classesToGenerate
+      classesToGenerate
     }
   }
 
@@ -491,14 +490,6 @@ internal class InjectedClassFirGenerator(session: FirSession, compatContext: Com
             classKind = classKind,
           ) {
             copyTypeParametersFrom(injectedClass.classSymbol, session)
-
-            if (!injectedClass.isAssistedInject) {
-              superType { typeParameterRefs ->
-                Symbols.ClassIds.metroFactory.constructClassLikeType(
-                  arrayOf(owner.constructType(typeParameterRefs))
-                )
-              }
-            }
           }
           .apply {
             markAsDeprecatedHidden(session)
@@ -553,22 +544,16 @@ internal class InjectedClassFirGenerator(session: FirSession, compatContext: Com
       return emptySet()
     }
 
-    val names = mutableSetOf<Name>()
-    names += SpecialNames.INIT
-
-    // Factory class
-    // Factory (companion) object
-    if (isFactoryClass) {
-      // Only generate an invoke() function if it has assisted parameters, as it won't be inherited
-      // from Factory<T> in this case
-      val target = injectFactoryClassIdsToInjectedClass[classSymbol.classId]
-      if (target?.isAssistedInject == true) {
-        names += Symbols.Names.invoke
-      }
+    if (isFactoryClass && !isFactoryCreatorClass) {
+      // Factory class constructor and invoke() are implemented in IR
+      return emptySet()
     }
-    if (isFactoryCreatorClass) {
-      names += Symbols.Names.create
-      names += Symbols.Names.newInstance
+
+    val names = mutableSetOf<Name>()
+
+    if (isObject || !isFactoryClass) {
+      // Don't add constructors for regular factory classes, they are added in IR
+      names += SpecialNames.INIT
     }
 
     // MembersInjector class
@@ -636,12 +621,7 @@ internal class InjectedClassFirGenerator(session: FirSession, compatContext: Com
         val injectedClass =
           injectFactoryClassIdsToInjectedClass[context.owner.classId] ?: return emptyList()
         injectedClass.populateAncestorMemberInjections(session)
-        buildFactoryConstructor(
-          context = context,
-          instanceReceiver = null,
-          extensionReceiver = null,
-          valueParameters = injectedClass.allParameters.dedupeParameters(session),
-        )
+        return emptyList()
       } else if (context.owner.hasOrigin(Keys.MembersInjectorClassDeclaration)) {
         val injectedClass =
           membersInjectorClassIdsToInjectedClass[context.owner.classId] ?: return emptyList()
@@ -739,69 +719,7 @@ internal class InjectedClassFirGenerator(session: FirSession, compatContext: Com
       val injectedClass = injectFactoryClassIdsToInjectedClass[targetClassId] ?: return emptyList()
 
       injectedClass.populateAncestorMemberInjections(session)
-
-      val returnType = injectedClass.classSymbol.defaultType()
-      functions +=
-        when (callableId.callableName) {
-          Symbols.Names.invoke -> {
-            // Assisted types do not inherit from Factory<T>, so we need to generate invoke here
-            createMemberFunction(
-                owner = nonNullContext.owner,
-                key = Keys.Default,
-                name = callableId.callableName,
-                returnTypeProvider = {
-                  injectedClass.classSymbol.constructType(
-                    nonNullContext.owner.typeParameterSymbols.mapToArray(
-                      FirTypeParameterSymbol::toConeType
-                    )
-                  )
-                },
-              ) {
-                injectedClass.assistedParameters.forEach { assistedParameter ->
-                  valueParameter(
-                    assistedParameter.name,
-                    assistedParameter.symbol.resolvedReturnType,
-                    key = Keys.RegularParameter,
-                  )
-                }
-              }
-              .symbol as FirNamedFunctionSymbol
-          }
-          Symbols.Names.create -> {
-            buildFactoryCreateFunction(
-              nonNullContext,
-              { typeParams ->
-                if (injectedClass.isAssistedInject) {
-                  targetClass.constructType(typeParams.mapToArray(FirTypeParameterRef::toConeType))
-                } else {
-                  Symbols.ClassIds.metroFactory.constructClassLikeType(
-                    arrayOf(
-                      injectedClass.classSymbol.constructType(
-                        typeParams.mapToArray(FirTypeParameterRef::toConeType)
-                      )
-                    )
-                  )
-                }
-              },
-              null,
-              null,
-              injectedClass.allParameters.dedupeParameters(session),
-            )
-          }
-          Symbols.Names.newInstance -> {
-            buildNewInstanceFunction(
-              nonNullContext,
-              Symbols.Names.newInstance,
-              returnType,
-              null,
-              null,
-              injectedClass.constructorParameters,
-            )
-          }
-          else -> {
-            reportCompilerBug("Unrecognized function $callableId")
-          }
-        }
+      // Rest is generated in IR
     } else if (targetClass.hasOrigin(Keys.MembersInjectorClassDeclaration)) {
       val injectedClass =
         membersInjectorClassIdsToInjectedClass[targetClassId] ?: return emptyList()

commit 271b76b48f3ad821c2336831894898d3b1f9c8bd

Fix assisted invoke parameter type wrapping in IR factory
This is a regression from 85543469edb769d13099c2b49608b38d8baf2c90

compiler/src/main/kotlin/dev/zacsweers/metro/compiler/ir/transformers/InjectedClassTransformer.kt
@@ -268,9 +268,13 @@ internal class InjectedClassTransformer(
           if (!isAssistedInject) {
             overriddenSymbols = listOf(metroSymbols.providerInvoke)
           } else {
+            val assistedInvokeParamTypeRemapper =
+              declaration.deepRemapperFor(factoryCls.defaultType)
             // Add assisted params
             for (param in constructorParameters.allParameters.filter { it.isAssisted }) {
-              addValueParameter(param.name, param.type)
+              val assistedParamType =
+                assistedInvokeParamTypeRemapper.remapType(param.contextualTypeKey.toIrType())
+              addValueParameter(param.name, assistedParamType)
             }
           }
         }

compiler-tests/src/test/data/box/interop/dagger/AssistedDaggerLazyParameterInteropRegression.kt
@@ -1,0 +1,34 @@
+// ENABLE_DAGGER_INTEROP
+package test
+
+import dagger.Lazy
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+
+// Regression repro for commit 85543469: assisted dagger.Lazy<T> params can be remapped to T,
+// causing a ClassCastException when the factory receives a DaggerInteropDoubleCheck instance.
+class AssistedLazyConsumer
+@AssistedInject
+constructor(@Assisted private val lazyString: Lazy<String>) {
+
+  fun value(): String = lazyString.get()
+
+  @AssistedFactory
+  interface Factory {
+    fun create(lazyString: Lazy<String>): AssistedLazyConsumer
+  }
+}
+
+@DependencyGraph
+interface ExampleGraph {
+  val assistedLazyConsumerFactory: AssistedLazyConsumer.Factory
+}
+
+fun box(): String {
+  val graph = createGraph<ExampleGraph>()
+  val consumer = graph.assistedLazyConsumerFactory.create(Lazy { "test" })
+
+  assertEquals("test", consumer.value())
+  return "OK"
+}

compiler-tests/src/test/java/dev/zacsweers/metro/compiler/BoxTestGenerated.java
@@ -1740,6 +1740,12 @@ public class BoxTestGenerated extends AbstractBoxTest {
       }
 
       @Test
+      @TestMetadata("AssistedDaggerLazyParameterInteropRegression.kt")
+      public void testAssistedDaggerLazyParameterInteropRegression() {
+        runTest("compiler-tests/src/test/data/box/interop/dagger/AssistedDaggerLazyParameterInteropRegression.kt");
+      }
+
+      @Test
       @TestMetadata("BindsOptionalInAMultibindingCycle.kt")
       public void testBindsOptionalInAMultibindingCycle() {
         runTest("compiler-tests/src/test/data/box/interop/dagger/BindsOptionalInAMultibindingCycle.kt");

compiler-tests/src/test/java/dev/zacsweers/metro/compiler/FastInitBoxTestGenerated.java
@@ -1740,6 +1740,12 @@ public class FastInitBoxTestGenerated extends AbstractFastInitBoxTest {
       }
 
       @Test
+      @TestMetadata("AssistedDaggerLazyParameterInteropRegression.kt")
+      public void testAssistedDaggerLazyParameterInteropRegression() {
+        runTest("compiler-tests/src/test/data/box/interop/dagger/AssistedDaggerLazyParameterInteropRegression.kt");
+      }
+
+      @Test
       @TestMetadata("BindsOptionalInAMultibindingCycle.kt")
       public void testBindsOptionalInAMultibindingCycle() {
         runTest("compiler-tests/src/test/data/box/interop/dagger/BindsOptionalInAMultibindingCycle.kt");