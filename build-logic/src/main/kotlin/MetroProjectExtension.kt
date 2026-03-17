54085c4fc9f7f619d3ed8ba4f362c91c3d94176a
Zac Sweers <pandanomic@gmail.com>
2026-03-17 14:28:34 -0400

    Refactor KMP configs to build logic

--- build-logic/src/main/kotlin/MetroProjectExtension.kt
+++ build-logic/src/main/kotlin/MetroProjectExtension.kt
@@ -1,11 +1,21 @@
 // Copyright (C) 2026 Zac Sweers
 // SPDX-License-Identifier: Apache-2.0
 import javax.inject.Inject
+import kotlin.text.set
+import org.gradle.api.Project
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JsModuleKind.MODULE_UMD
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
+import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
 
-abstract class MetroProjectExtension @Inject constructor(objects: ObjectFactory) {
+abstract class MetroProjectExtension
+@Inject
+constructor(private val project: Project, objects: ObjectFactory) {
   abstract val jvmTarget: Property<String>
   val languageVersion: Property<KotlinVersion> =
     objects.property(KotlinVersion::class.java).convention(KotlinVersion.DEFAULT)
@@ -15,4 +25,150 @@ abstract class MetroProjectExtension @Inject constructor(objects: ObjectFactory)
     objects
       .property(Boolean::class.java)
       .convention(languageVersion.map { it < KotlinVersion.DEFAULT })
+
+  /*
+   * Here's the main hierarchy of variants. Any `expect` functions in one level of the tree are
+   * `actual` functions in a (potentially indirect) child node.
+   *
+   * ```
+   *   common
+   *   |-- jvm
+   *   |-- js
+   *   '-- native
+   *       |- unix
+   *       |   |-- apple
+   *       |   |   |-- iosArm64
+   *       |   |   |-- iosX64
+   *       |   |   |-- tvosArm64
+   *       |   |   |-- watchosArm32
+   *       |   |   |-- watchosArm64
+   *       |   |   '-- watchosX86
+   *       |   '-- linux
+   *       |       '-- linuxX64
+   *       '-- mingw
+   *           '-- mingwX64
+   * ```
+   *
+   * Every child of `unix` also includes a source set that depends on the pointer size:
+   *
+   *  * `sizet32` for watchOS, including watchOS 64-bit architectures
+   *  * `sizet64` for everything else
+   */
+  fun configureCommonKmpTargets(
+    jsModuleName: String,
+    includeAndroid: Boolean = false, // TODO
+    isComposeTarget: Boolean = false,
+  ) {
+    project.pluginManager.withPlugin("org.jetbrains.kotlin.multiplatform") {
+      // Sourced from https://kotlinlang.org/docs/native-target-support.html
+      with(project.kotlinExtension as KotlinMultiplatformExtension) {
+        jvm()
+        js(IR) {
+          outputModuleName.set("$jsModuleName-js")
+          compilations.configureEach {
+            compileTaskProvider.configure {
+              compilerOptions {
+                moduleKind.set(MODULE_UMD)
+                sourceMap.set(true)
+              }
+            }
+          }
+          nodejs { testTask { useMocha { timeout = "30s" } } }
+          browser()
+          binaries.executable()
+        }
+
+        @OptIn(ExperimentalWasmDsl::class)
+        wasmJs {
+          outputModuleName.set("$jsModuleName-wasmjs")
+          binaries.executable()
+          browser {}
+        }
+
+        @OptIn(ExperimentalWasmDsl::class)
+        wasmWasi {
+          binaries.executable()
+          nodejs()
+        }
+
+        /////// Native targets
+        if (isComposeTarget) {
+          // Compose-supported native targets
+          iosArm64()
+          iosSimulatorArm64()
+          iosX64()
+          macosArm64()
+          macosX64()
+        } else {
+          // Tier 1
+          iosArm64()
+          iosSimulatorArm64()
+          macosArm64()
+
+          // Tier 2
+          linuxArm64()
+          linuxX64()
+          tvosArm64()
+          tvosSimulatorArm64()
+          watchosArm32()
+          watchosArm64()
+          watchosSimulatorArm64()
+
+          // Tier 3
+          androidNativeArm32()
+          androidNativeArm64()
+          androidNativeX64()
+          androidNativeX86()
+          iosX64()
+          macosX64()
+          mingwX64()
+          tvosX64()
+          watchosDeviceArm64()
+          watchosX64()
+        }
+
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        applyDefaultHierarchyTemplate {
+          common {
+            group("wasm") {
+              withWasmJs()
+              withWasmWasi()
+            }
+            group("web") {
+              withJs()
+              withWasmJs()
+              withWasmWasi()
+            }
+            group("nonWeb") {
+              withJvm()
+              withNative()
+            }
+          }
+        }
+
+        targets
+          .matching {
+            it.platformType == KotlinPlatformType.js || it.platformType == KotlinPlatformType.wasm
+          }
+          .configureEach {
+            val target = this
+            compilations.configureEach {
+              compileTaskProvider.configure {
+                compilerOptions {
+                  freeCompilerArgs.add(
+                    "-Xklib-duplicated-unique-name-strategy=allow-all-with-warning"
+                  )
+                  if (target.platformType == KotlinPlatformType.js) {
+                    freeCompilerArgs.add(
+                      // These are all read at compile-time
+                      "-Xwarning-level=RUNTIME_ANNOTATION_NOT_SUPPORTED:disabled"
+                    )
+                  }
+                }
+              }
+            }
+          }
+      }
+    }
+  }
 }
--- metrox-viewmodel/build.gradle.kts
+++ metrox-viewmodel/build.gradle.kts
@@ -1,73 +1,16 @@
 // Copyright (C) 2025 Zac Sweers
 // SPDX-License-Identifier: Apache-2.0
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
-import org.jetbrains.kotlin.gradle.dsl.JsModuleKind.MODULE_UMD
 import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
 
 plugins {
   alias(libs.plugins.kotlin.multiplatform)
+  id("metro.base")
   id("metro.publish")
 }
 
+metroProject { configureCommonKmpTargets("metrox-viewmodel") }
+
 kotlin {
-  jvm()
-  js(IR) {
-    outputModuleName = "metrox-viewmodel-js"
-    compilations.configureEach {
-      compileTaskProvider.configure {
-        compilerOptions {
-          moduleKind.set(MODULE_UMD)
-          sourceMap.set(true)
-        }
-      }
-    }
-    nodejs { testTask { useMocha { timeout = "30s" } } }
-    browser()
-    binaries.executable()
-  }
-
-  @OptIn(ExperimentalWasmDsl::class)
-  wasmJs {
-    outputModuleName = "metrox-viewmodel-wasmjs"
-    binaries.executable()
-    browser {}
-  }
-
-  // Native targets supported by lifecycle-viewmodel
-  // Tier 1
-  iosArm64()
-  iosSimulatorArm64()
-  iosX64()
-  linuxX64()
-  macosArm64()
-  macosX64()
-
-  // Tier 2
-  linuxArm64()
-  tvosArm64()
-  tvosSimulatorArm64()
-  tvosX64()
-  watchosArm32()
-  watchosArm64()
-  watchosSimulatorArm64()
-  watchosX64()
-
-  @OptIn(ExperimentalKotlinGradlePluginApi::class)
-  applyDefaultHierarchyTemplate {
-    common {
-      group("web") {
-        withJs()
-        withWasmJs()
-        withWasmWasi()
-      }
-      group("nonWeb") {
-        withJvm()
-        withNative()
-      }
-    }
-  }
-
   sourceSets {
     commonMain {
       dependencies {
