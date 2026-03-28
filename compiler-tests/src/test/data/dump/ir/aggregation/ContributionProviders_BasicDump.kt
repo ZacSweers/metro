2fce15498741403ab28f8e9725c2052e4279cf75
Zac Sweers <pandanomic@gmail.com>
2026-03-28 16:48:02 -0400

    Broaden dump tests

--- compiler-tests/src/test/data/dump/ir/aggregation/ContributionProviders_BasicDump.kt
+++ compiler-tests/src/test/data/dump/ir/aggregation/ContributionProviders_BasicDump.kt
@@ -1,10 +1,17 @@
 // GENERATE_CONTRIBUTION_PROVIDERS
 // GENERATE_CONTRIBUTION_HINTS: true
 // GENERATE_CONTRIBUTION_HINTS_IN_FIR
+// ENABLE_FUNCTION_PROVIDERS
 // COMPILER_VERSION: 2.3.20
 
 interface Base
 
 @ContributesBinding(AppScope::class)
 @Inject
-internal class Impl(val input: String) : Base
+internal class Impl(
+  val string: String,
+  val longProvider: Provider<Long>,
+  val longFun: () -> Long,
+  val lazyBool: Lazy<Boolean>,
+  val providerOfLazyDouble: Provider<Lazy<Double>>,
+) : Base
