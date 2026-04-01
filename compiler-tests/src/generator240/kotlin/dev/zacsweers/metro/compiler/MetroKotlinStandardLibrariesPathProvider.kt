15b8e966fcf2074997d1b563cbd75efad0b10557
Zac Sweers <pandanomic@gmail.com>
2026-04-01 13:52:11 -0400

    2.4.0-Beta1 test infra

--- compiler-tests/src/generator240/kotlin/dev/zacsweers/metro/compiler/MetroKotlinStandardLibrariesPathProvider.kt
+++ compiler-tests/src/generator240/kotlin/dev/zacsweers/metro/compiler/MetroKotlinStandardLibrariesPathProvider.kt
@@ -3,12 +3,18 @@
 package dev.zacsweers.metro.compiler
 
 import java.io.File
+import org.jetbrains.kotlin.platform.wasm.WasmTarget
 import org.jetbrains.kotlin.test.services.KotlinStandardLibrariesPathProvider
 
 // KotlinStandardLibrariesPathProvider was changed to an interface in 2.3.20
-// webStdlibForTests() was added as a new abstract member in 2.3.20-RC
+// fullWasmStdlib() was added as a new abstract member in 2.4.0-Beta1
+// kotlinTestWasmKLib() was added as a new abstract member in 2.4.0-Beta1
 abstract class MetroKotlinStandardLibrariesPathProvider : KotlinStandardLibrariesPathProvider {
   abstract override fun webStdlibForTests(): File
+
+  override fun fullWasmStdlib(target: WasmTarget): File = fullWasmStdlib(target.alias)
+
+  override fun kotlinTestWasmKLib(target: WasmTarget): File = kotlinTestWasmKLib(target.alias)
 
   abstract fun fullWasmStdlib(target: String): File
 
