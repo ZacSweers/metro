156a42b355b4de7e95ca694b9a75b464d95577c4
Jon Amireh <2724407+jonamireh@users.noreply.github.com>
2026-04-16 16:48:28 -0700

    Add failing test for secondary constructor with contributionproviders

---
+++ compiler-tests/src/test/data/box/aggregation/contributionproviders/secondaryConstructor.kt
@@ -0,0 +1,19 @@
+// GENERATE_CONTRIBUTION_PROVIDERS
+
+@DependencyGraph(AppScope::class)
+interface AppGraph {
+    val accountManager: AccountManager
+}
+
+interface AccountManager
+
+@ContributesBinding(AppScope::class)
+class RealAccountManager : AccountManager {
+    @Inject
+    constructor()
+}
+
+fun box(): String {
+    val graph = createGraph<AppGraph>()
+    return "OK"
+}
