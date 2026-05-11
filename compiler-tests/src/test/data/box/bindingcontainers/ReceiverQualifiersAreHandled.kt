3d71649d397a6f40f196cf17f68a217c434264dd
Zac Sweers <pandanomic@gmail.com>
2026-05-11 13:59:10 -0400

    Expand

--- compiler-tests/src/test/data/box/bindingcontainers/ReceiverQualifiersAreHandled.kt
+++ compiler-tests/src/test/data/box/bindingcontainers/ReceiverQualifiersAreHandled.kt
@@ -1,14 +1,17 @@
 // https://github.com/ZacSweers/metro/discussions/2265
 @DependencyGraph
 interface AppGraph {
-  val value: Number
+  @Named("functionValue") val functionValue: Number
+  @Named("propertyValue") val propertyValue: Number
 
   @Named("int") @Provides fun provideInt(): Int = 3
-  @Binds fun @receiver:Named("int") Int.bindNumber(): Number
+  @Named("functionValue") @Binds fun @receiver:Named("int") Int.bindNumber(): Number
+  @Named("propertyValue") @Binds val @receiver:Named("int") Int.bindNumber: Number
 }
 
 fun box(): String {
   val graph = createGraph<AppGraph>()
-  assertEquals(3, graph.value)
+  assertEquals(3, graph.functionValue)
+  assertEquals(3, graph.propertyValue)
   return "OK"
 }
