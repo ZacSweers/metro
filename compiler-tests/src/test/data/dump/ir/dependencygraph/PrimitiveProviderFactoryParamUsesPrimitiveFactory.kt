4cdd45995ab382325437a51d6e86125c832c28ec
Zac Sweers <pandanomic@gmail.com>
2026-06-03 10:51:57 -0400

    Update tests for not always keeping instance factories

--- compiler-tests/src/test/data/dump/ir/dependencygraph/PrimitiveProviderFactoryParamUsesPrimitiveFactory.kt
+++ compiler-tests/src/test/data/dump/ir/dependencygraph/PrimitiveProviderFactoryParamUsesPrimitiveFactory.kt
@@ -2,9 +2,11 @@
 interface AppGraph {
   val int: Int
   val intProvider: () -> Int
+  val longProvider: () -> Long
+  val longProvider2: () -> Long
 
   @DependencyGraph.Factory
   interface Factory {
-    fun create(@Provides int: Int): AppGraph
+    fun create(@Provides int: Int, @Provides long: Long): AppGraph
   }
 }
