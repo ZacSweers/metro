90839371dbf282adc77f7fc73a5ab78187785741
Zac Sweers <pandanomic@gmail.com>
2026-08-05 10:55:36 -0400

    Add another graphprivate test

--- compiler-tests/src/test/java/dev/zacsweers/metro/compiler/FastInitBoxTestGenerated.java
+++ compiler-tests/src/test/java/dev/zacsweers/metro/compiler/FastInitBoxTestGenerated.java
@@ -3627,6 +3627,12 @@ public class FastInitBoxTestGenerated extends AbstractFastInitBoxTest {
     }
 
     @Test
+    @TestMetadata("NestedGraphExtensionMiddleScopedBinding.kt")
+    public void testNestedGraphExtensionMiddleScopedBinding() {
+      run("NestedGraphExtensionMiddleScopedBinding.kt");
+    }
+
+    @Test
     @TestMetadata("NestedSuspendFunctionWrappersWithoutRuntime.kt")
     public void testNestedSuspendFunctionWrappersWithoutRuntime() {
       run("NestedSuspendFunctionWrappersWithoutRuntime.kt");
