a0486ade2acf198cb76848989b61a415741d0df0
bradley <bradley@monzo.com>
2026-08-12 20:30:59 +0100

    Add failing test case

---
+++ compiler-tests/src/test/data/box/inject/member/DaggerInteropDeeplyNestedClassNameExceedsFileLengthLimit.kt
@@ -0,0 +1,23 @@
+// ENABLE_DAGGER_INTEROP
+// GENERATE_CLASSES_IN_IR: true
+
+class OuterClassWithADeliberatelyLongNameToOverflowMetrosLimit {
+  class MiddleClassWithADeliberatelyLongNameToOverflowMetrosLimit {
+    class InnerClassWithADeliberatelyLongNameToOverflowMetrosLimit {
+      class NestedClassWithADeliberatelyLongNameToOverflowMetrosLimit
+    }
+  }
+}
+
+@DependencyGraph
+interface AppGraph {
+  val int: Int
+
+  @Provides fun provideInt(): Int = 3
+}
+
+fun box(): String {
+  val graph = createGraph<AppGraph>()
+  assertEquals(3, graph.int)
+  return "OK"
+}
