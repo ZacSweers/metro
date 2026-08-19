f403244f2a247361cd70c2f4ca047e6d40f71767
Zac Sweers <pandanomic@gmail.com>
2026-08-19 00:36:52 -0400

    Move graph debug export to the overflow menu

--- idea-plugin/src/main/kotlin/dev/zacsweers/metro/idea/toolwindow/MetroToolWindowPanel.kt
+++ idea-plugin/src/main/kotlin/dev/zacsweers/metro/idea/toolwindow/MetroToolWindowPanel.kt
@@ -105,6 +105,11 @@ internal class MetroToolWindowPanel(private val project: Project) :
       }
     )
 
+    val overflowGroup =
+      DefaultActionGroup("More", true).apply {
+        templatePresentation.icon = AllIcons.Actions.More
+        add(ExportGraphDebugInfoAction(project) { selectedGraphNode()?.context })
+      }
     val actionGroup =
       DefaultActionGroup(
         // Not DumbAware: the refreshed tree needs stub indexes, so wait for smart mode
@@ -126,7 +131,7 @@ internal class MetroToolWindowPanel(private val project: Project) :
             selectedGraphNode()?.context?.let(::validateContext)
           }
         },
-        ExportGraphDebugInfoAction(project) { selectedGraphNode()?.context },
+        overflowGroup,
       )
     val toolbar =
       ActionManager.getInstance().createActionToolbar("MetroToolWindow", actionGroup, true)
