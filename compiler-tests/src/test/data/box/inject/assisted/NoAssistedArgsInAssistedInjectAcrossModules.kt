3b9f829c8a063517f5d1761300bb1f2e41d43017
bradley <bradley@monzo.com>
2026-08-19 16:38:02 +0100

    @AssistedInject with no assisted params runtime crash

---
+++ compiler-tests/src/test/data/box/inject/assisted/NoAssistedArgsInAssistedInjectAcrossModules.kt
@@ -0,0 +1,21 @@
+// GENERATE_CLASSES_IN_IR: true
+// MODULE: lib
+@AssistedInject
+class Example {
+  @AssistedFactory
+  interface Factory {
+    fun create(): Example
+  }
+}
+
+// MODULE: main(lib)
+@DependencyGraph
+interface AppGraph {
+  val factory: Example.Factory
+}
+
+fun box(): String {
+  val instance = createGraph<AppGraph>().factory.create()
+  assertNotNull(instance)
+  return "OK"
+}
