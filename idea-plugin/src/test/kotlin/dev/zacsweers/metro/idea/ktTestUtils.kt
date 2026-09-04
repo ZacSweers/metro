8e9685cbc0c39669a9f1789ae5d65bc2677a57d3
Zac Sweers <1361086+ZacSweers@users.noreply.github.com>
2026-09-04 12:27:44 -0400

    Update test compiler options under a write lock

--- idea-plugin/src/test/kotlin/dev/zacsweers/metro/idea/ktTestUtils.kt
+++ idea-plugin/src/test/kotlin/dev/zacsweers/metro/idea/ktTestUtils.kt
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 package dev.zacsweers.metro.idea
 
+import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.application.smartReadAction
 import com.intellij.openapi.components.service
 import com.intellij.openapi.module.Module
@@ -12,6 +13,7 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.testFramework.PlatformTestUtil
 import com.intellij.testFramework.fixtures.CodeInsightTestFixture
+import com.intellij.testFramework.runInEdtAndWait
 import com.intellij.util.WaitFor
 import dev.zacsweers.metro.idea.graph.KaGraphValidationResult
 import dev.zacsweers.metro.idea.index.AutomaticRefreshWindow
@@ -155,6 +157,7 @@ internal fun Project.enableImmediateAutomaticRefresh() {
   service<MetroResolutionService>().settingsChanged()
 }
 
+/** Configures Metro for the fixture, enabling the plugin unless explicitly disabled. */
 internal fun Project.setMetroOptions(vararg options: Pair<String, String>) {
   val configuredOptions =
     if (options.any { (name, _) -> name == "enabled" }) {
@@ -162,15 +165,25 @@ internal fun Project.setMetroOptions(vararg options: Pair<String, String>) {
     } else {
       listOf("enabled" to "true") + options
     }
-  KotlinCommonCompilerArgumentsHolder.getInstance(this).update {
-    pluginOptions =
-      configuredOptions.map { (name, value) -> "plugin:$PLUGIN_ID:$name=$value" }.toTypedArray()
-  }
+  updateMetroOptions(
+    configuredOptions.map { (name, value) -> "plugin:$PLUGIN_ID:$name=$value" }.toTypedArray()
+  )
 }
 
 /** Removes the plugin arguments so tests can represent a project that does not use Metro. */
 internal fun Project.clearMetroOptions() {
-  KotlinCommonCompilerArgumentsHolder.getInstance(this).update { pluginOptions = null }
+  updateMetroOptions(null)
+}
+
+/**
+ * Holds write access through option changes and notifications so background reads see one state.
+ */
+private fun Project.updateMetroOptions(options: Array<String>?) {
+  runInEdtAndWait {
+    runWriteAction {
+      KotlinCommonCompilerArgumentsHolder.getInstance(this).update { pluginOptions = options }
+    }
+  }
 }
 
 /**
