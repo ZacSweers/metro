9579d626c3645934ea4429a040a2b9dfd44f0102
Zac Sweers <pandanomic@gmail.com>
2026-09-16 18:08:49 -0400

    Make graph-viewer impl

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -139,7 +139,7 @@ tasks.withType<AndroidLintAnalysisTask>().configureEach { dependsOn(shadowJar) }
 tasks.withType<LintModelWriterTask>().configureEach { dependsOn(shadowJar) }
 
 dependencies {
-  api(project(":graph-viewer"))
+  implementation(project(":graph-viewer"))
   compileOnly(libs.kotlin.gradlePlugin)
   compileOnly(libs.kotlin.gradlePlugin.api)
   compileOnly(libs.kotlin.stdlib)
